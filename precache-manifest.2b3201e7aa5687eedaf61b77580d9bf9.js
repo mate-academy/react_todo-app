d98216f109d4db4a4b346eb4a673a7e8844a79d5
mgrinko <glukym@gmail.com>
2019-06-14 22:01:26 +0300

    Updates

---
+++ precache-manifest.2b3201e7aa5687eedaf61b77580d9bf9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "686f9d3c14f09b49a5ca8ca4f0f1638b",
+    "url": "/react-jun19-todo-app/index.html"
+  },
+  {
+    "revision": "b0c6e7109daa02475c82",
+    "url": "/react-jun19-todo-app/static/css/main.095e198b.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/react-jun19-todo-app/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "b0c6e7109daa02475c82",
+    "url": "/react-jun19-todo-app/static/js/main.c1b5a3f9.chunk.js"
+  },
+  {
+    "revision": "5387005ef19e81b979cf",
+    "url": "/react-jun19-todo-app/static/js/runtime~main.fca6f6d6.js"
+  }
+]);
