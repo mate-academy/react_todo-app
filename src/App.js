3d213f8db5d9914270e3e891ef979c84743aabfd
mgrinko <glukym@gmail.com>
2019-06-14 22:02:34 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <section className="todoapp">
+      <header className="header">
+        <h1>todos</h1>
+
+        <input
+          className="new-todo"
+          placeholder="What needs to be done?"
+          autoFocus=""
+        />
       </header>
-    </div>
+
+      <section className="main" style={{ display: 'block' }}>
+        <input id="toggle-all" className="toggle-all" type="checkbox" />
+        <label htmlFor="toggle-all">Mark all as complete</label>
+        <ul className="todo-list">
+          <li className="">
+            <div className="view">
+              <input className="toggle" type="checkbox" />
+              <label>sdfsdfsdf</label>
+              <button className="destroy"></button>
+            </div>
+          </li>
+          <li className="">
+            <div className="view">
+              <input className="toggle" type="checkbox" />
+              <label>dsfgsdfgdsrg</label>
+              <button className="destroy"></button></div>
+          </li>
+          <li className="">
+            <div className="view">
+              <input className="toggle" type="checkbox" />
+              <label>sddfgdfgdf</label>
+              <button className="destroy"></button>
+            </div>
+          </li>
+        </ul>
+      </section>
+      <footer className="footer" style={{ display: 'block' }}>
+        <span className="todo-count"><strong>3</strong> items left</span>
+        <ul className="filters">
+          <li>
+            <a href="#/" className="selected">All</a>
+          </li>
+          <li>
+            <a href="#/active">Active</a>
+          </li>
+          <li>
+            <a href="#/completed">Completed</a>
+          </li>
+        </ul>
+        <button className="clear-completed" style={{ display: 'block' }}></button>
+      </footer>
+    </section>
   );
 }
 
