32cf180c50bc796b89e12df4d68f30aacb4a2b2a
mgrinko <glukym@gmail.com>
2019-07-04 09:13:20 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_todo-app/precache-manifest.c5d215897eab9b0ceddf2568b68742c9.js"
+  "/react_todo-app/precache-manifest.36c0f0e73e4ba58e6c71741bdc35771c.js"
 );
 
 self.addEventListener('message', (event) => {
