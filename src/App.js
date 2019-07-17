6bbb57bd775730dd7de9b26c7a69b40d1667b74b
Serhii <davydko.serhiy@gmail.com>
2019-07-17 17:08:17 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import Todo from './Todo'
+import Todo from './Todo';
 
+/*
 const myStorage = window.localStorage;
 
 window.onbeforeunload = closingCode;
@@ -9,147 +10,200 @@ function closingCode() {
   return null;
 }
 
+ */
+
 class App extends React.Component {
   state = {
     allToggled: false,
-    showContent: true,
-    todoes: [{
-      id: '1',
-      title: '11111',
-      isDone: false,
-    },
-    {
-      id: '2',
-      title: '2222',
-      isDone: false,
-    },
-    {
-      id: '3',
-      title: '33333',
-      isDone: false,
-    },
-    ],
+    showContent: false,
+    value: '',
+    todoes: [],
   }
 
-  componentDidMount() {
-    const myLocaleStorage = myStorage.getItem('todoes');
-    if (myLocaleStorage === 'undefined') {
-      this.setState({
-        todoes: JSON.parse(myLocaleStorage)
-      })
+  /*   componentDidMount() {
+      const myLocaleStorage = myStorage.getItem('todoes');
+
+      if (myLocaleStorage === 'undefined') {
+        this.setState({
+          todoes: JSON.parse(myLocaleStorage),
+        })
+      }
     }
-  }
 
-  componentWillUnmount() {
-    myStorage.setItem('todoes', JSON.stringify(this.state.todoes));
-  }
+    componentWillUnmount() {
+      myStorage.setItem('todoes', JSON.stringify(this.state.todoes));
+    } */
 
   toggle = (id) => {
     this.setState(prevState => ({
-      todoes: prevState.todoes.map(todo => {
+      todoes: prevState.todoes.map((todo) => {
         if (todo.id !== id) {
-          return todo
+          return todo;
         }
-        else {
-          return {
-            ...todo,
-            isDone: !todo.isDone,
-          }
-        }
-      }
-      )
-    }))
+
+        return {
+          ...todo,
+          isDone: !todo.isDone,
+        };
+      }),
+    }));
   }
 
   toggleAll = () => {
     this.setState(prevState => ({
-      todoes: prevState.todoes.map(todo => {
-        if (prevState.allToggled===false) {
+      todoes: prevState.todoes.map((todo) => {
+        if (prevState.allToggled === false) {
           return {
             ...todo,
             isDone: true,
-          }
-        } else {
-          return {
-            ...todo,
-            isDone: false,
-          }
+          };
         }
 
-      }
-      ),
-      allToggled: !prevState.allToggled
-    }))
+        return {
+          ...todo,
+          isDone: false,
+        };
+      }),
+      allToggled: !prevState.allToggled,
+    }));
   }
 
-  destroy =(id) => {
+  destroy = (id) => {
     this.setState(prevState => ({
-      todoes: prevState.todoes.filter(todo =>todo.id!==id),
-      showContent: prevState.todoes.length < 2 ? false : true,
-    }))
+      todoes: prevState.todoes.filter(todo => todo.id !== id),
+      showContent: !(prevState.todoes.length < 2),
+    }));
   }
+
+  handleChange = (event) => {
+    this.setState({
+      value: event.target.value,
+    });
+  }
+
+  /*   handleFilterDone = () => {
+      this.setState(prevState => ({
+        todoes: prevState.todoes.filter(todo => (
+          todo.isDone
+        )),
+      }))
+    }
+
+    handleFilterNotDone = () => {
+      this.setState(prevState => ({
+        todoes: prevState.todoes.filter(todo => (
+          !todo.isDone
+        )),
+      }))
+    }
+   */
 
   render() {
-    console.log(this.state.todoes);
-    const { showContent, todoes } = this.state;
-    const resultingList = (todoes.length !== 0) ? todoes.map(todo => (
-      <Todo todo={todo} toggle={this.toggle} destroy = {this.destroy} />
-    )) : '';
+    const { showContent, todoes, value } = this.state;
+    const resultingList = todoes.length !== 0
+      ? todoes.map(todo => (
+        <Todo todo={todo} toggle={this.toggle} destroy={this.destroy} />
+      ))
+      : '';
+
     return (
       <section className="todoapp">
         <header className="header">
           <h1>todos</h1>
+          <form onSubmit={event => event.preventDefault()}>
+            <input
+              className="new-todo"
+              onChange={this.handleChange}
+              value={value}
+              placeholder="What needs to be done?"
+              onKeyDown={(event) => {
+                if (event.key === 'Enter') {
+                  this.setState(prevState => ({
+                    todoes: [...prevState.todoes, {
+                      id: Date.now(),
+                      title: prevState.value,
+                      isDone: false,
+                    }],
+                    value: '',
+                    showContent: true,
+                  }));
+                }
+              }}
+            />
 
-          <input
-            className="new-todo"
-            placeholder="What needs to be done?"
-          />
+          </form>
+
         </header>
 
-        {showContent === true ? (<div className="content-group" >
+        {showContent === true
+          ? (
+            <div className="content-group">
 
-          <section className="main">
-            <input type="checkbox" id="toggle-all" className="toggle-all" onClick ={this.toggleAll}/ >
-            <label htmlFor="toggle-all">Mark all as complete</label>
+              <section className="main">
+                <input
+                  type="checkbox"
+                  id="toggle-all"
+                  className="toggle-all"
+                  onClick={this.toggleAll}
+                />
+                <label
+                  htmlFor="toggle-all"
+                >
+                  Mark all as complete
+                </label>
 
-            <ul className="todo-list">
+                <ul className="todo-list">
 
-              {resultingList}
+                  {resultingList}
 
-            </ul>
-          </section>
+                </ul>
+              </section>
 
-          <footer className="footer">
-            <span className="todo-count">
-              {`${todoes.length} items left`}
-</span>
+              <footer className="footer">
+                <span className="todo-count">
+                  {`${todoes.filter(todo => (!todo.isDone)).length} items left`}
+                </span>
 
-            <ul className="filters">
-              <li>
-                <a href="#/" className="selected">All</a>
-              </li>
+                <ul className="filters">
+                  <li>
+                    <a
+                      href="#/"
+                      className="selected"
+                    >
+                      All
+                    </a>
+                  </li>
 
-              <li>
-                <a href="#/active">Active</a>
-              </li>
+                  <li>
+                    <a
+                      href="#/active"
+                      onClick={() => this.handleFilterNotDone}
+                    >
+                      Active
+                    </a>
+                  </li>
 
-              <li>
-                <a href="#/completed">Completed</a>
-              </li>
-            </ul>
+                  <li>
+                    <a
+                      href="#/completed"
+                      onClick={() => this.handleFilterDone}
+                    >
+                      Completed
+                    </a>
+                  </li>
+                </ul>
 
-            <button
-              type="button"
-              className="clear-completed"
-              style={{ display: 'block' }}
-            />
-          </footer>
-        </div>) : <span></span>}
+                <button
+                  type="button"
+                  className="clear-completed"
+                  style={{ display: 'block' }}
+                />
+              </footer>
+            </div>)
+          : <span>''</span>}
       </section>
     );
-
   }
-
 }
 
 export default App;
--- src/Todo.js
+++ src/Todo.js
@@ -1,16 +1,48 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const Todo = ({ todo, toggle, destroy }) => {
   const { id, title, isDone } = todo;
+
   return (
     <li className="todo">
       <div className="view">
-        <input type="checkbox" className="toggle" id={id} onClick ={() => toggle(id)} checked = {isDone}/>
-        <label htmlFor={id} className ={isDone && "completed-item"}>{title}</label>
-        <button type="button" className="destroy" onClick ={() => destroy(id)} />
+        <input
+          type="checkbox"
+          className="toggle"
+          id={id}
+          onClick={() => toggle(id)}
+          checked={isDone}
+        />
+        <label
+          htmlFor={id}
+          className={isDone && 'completed-item'}
+        >
+          {title}
+        </label>
+        <button
+          type="button"
+          className="destroy"
+          onClick={() => destroy(id)}
+        />
       </div>
     </li>
-  )
-}
+  );
+};
 
-export default Todo
+Todo.propTypes = {
+  todo: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    idDone: PropTypes.bool.isRequired,
+  }).isRequired,
+  toggle: PropTypes.func,
+  destroy: PropTypes.func,
+};
+
+Todo.defaultProps = {
+  toggle: null,
+  destroy: null,
+};
+
+export default Todo;
