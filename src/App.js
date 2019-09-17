7838502171a138a336de99105e94acae213f32e1
yevhen-sahach <antitop04@gmail.com>
2019-09-18 01:14:48 +0300

    removed test todo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     super(props);
 
     this.state = {
-      listOfTodos: [{ title: 'test', isChecked: false }],
+      listOfTodos: [],
       filteredTodos: [],
       valueOfMainInput: '',
       activeFilter: 'all',
