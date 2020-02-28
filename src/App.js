080ac60c7ebefdcc7dde24dda4f6e83145970e7d
Valeriy-Sorochynskyi <haribol162@gmail.com>
2020-02-28 09:53:14 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -1,85 +1,224 @@
 import React from 'react';
+import uuid from 'react-uuid';
+import { TodoList } from './components/TodoList/TodoList';
 
-function App() {
-  return (
-    <section className="todoapp">
-      <header className="header">
-        <h1>todos</h1>
+class App extends React.Component {
+  state = {
+    todos: [
+      {
+        done: false, id: '1', text: 'do exercise',
+      },
+      {
+        done: true, id: '2', text: 'buy milk',
+      },
+    ],
+    newItemText: '',
+    checkedAll: false,
+    sortBy: '',
+  }
 
-        <input
-          className="new-todo"
-          placeholder="What needs to be done?"
-        />
-      </header>
+  checkAll = () => {
+    this.setState(({ todos, checkedAll }) => {
+      const newTodos = todos.map(todo => ({
+        ...todo,
+        done: !checkedAll,
+      }));
 
-      <section className="main">
-        <input type="checkbox" id="toggle-all" className="toggle-all" />
-        <label htmlFor="toggle-all">Mark all as complete</label>
+      return {
+        todos: newTodos,
+        checkedAll: !checkedAll,
+      };
+    });
+  }
 
-        <ul className="todo-list">
-          <li>
-            <div className="view">
-              <input type="checkbox" className="toggle" id="todo-1" />
-              <label htmlFor="todo-1">asdfghj</label>
-              <button type="button" className="destroy" />
-            </div>
-            <input type="text" className="edit" />
-          </li>
+  handleChange = ({ target }) => {
+    this.setState({
+      newItemText: target.value,
+    });
+  }
 
-          <li className="completed">
-            <div className="view">
-              <input type="checkbox" className="toggle" id="todo-2" />
-              <label htmlFor="todo-2">qwertyuio</label>
-              <button type="button" className="destroy" />
-            </div>
-            <input type="text" className="edit" />
-          </li>
+  handleDelete = (id) => {
+    this.setState(({ todos }) => ({
+      todos: todos.filter(current => current.id !== id),
+    }));
+  };
 
-          <li className="editing">
-            <div className="view">
-              <input type="checkbox" className="toggle" id="todo-3" />
-              <label htmlFor="todo-3">zxcvbnm</label>
-              <button type="button" className="destroy" />
-            </div>
-            <input type="text" className="edit" />
-          </li>
+  toggleItem = (id) => {
+    this.setState(({ todos }) => {
+      const newTodos = todos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            done: !todo.done,
+          };
+        }
 
-          <li>
-            <div className="view">
-              <input type="checkbox" className="toggle" id="todo-4" />
-              <label htmlFor="todo-4">1234567890</label>
-              <button type="button" className="destroy" />
-            </div>
-            <input type="text" className="edit" />
-          </li>
-        </ul>
+        return todo;
+      });
+
+      return {
+        todos: newTodos,
+      };
+    });
+  }
+
+  handleAddItem = (event) => {
+    event.preventDefault();
+    this.addItem(this.state.newItemText);
+  }
+
+  changeText = (id, text) => {
+    this.setState(({ todos }) => {
+      const newItems = todos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            text,
+          };
+        }
+
+        return todo;
+      });
+
+      return {
+        todos: newItems,
+      };
+    });
+  };
+
+  todosFilter = (todos, sortBy) => {
+    switch (sortBy) {
+      case 'all':
+        return todos;
+      case 'done':
+        return todos.filter(todo => todo.done);
+      case 'active':
+        return todos.filter(todo => !todo.done);
+      default:
+        return todos;
+    }
+  };
+
+  handleOnAll = () => {
+    this.setState({ sortBy: 'All' });
+  };
+
+  handleOnActive = () => {
+    this.setState({ sortBy: 'active' });
+  };
+
+  handleOnCompleted = () => {
+    this.setState({ sortBy: 'done' });
+  };
+
+  clearCompleted = () => {
+    this.setState(({ todos }) => ({
+      todos: todos.filter(todo => !todo.done),
+    }));
+  };
+
+  addItem(text) {
+    if (text === '') {
+      return;
+    }
+
+    this.setState(({ todos }) => {
+      const newItem = {
+        id: uuid(),
+        done: false,
+        text,
+      };
+
+      return {
+        todos: [...todos, newItem],
+        newItemText: '',
+      };
+    });
+  }
+
+  render() {
+    const { todos, newItemText, sortBy } = this.state;
+    const activeTodos = todos.filter(todo => !todo.done);
+    const filteredTodos = this.todosFilter(todos, sortBy);
+
+    return (
+      <section className="todoapp">
+        <header className="header">
+          <h1>todos</h1>
+          <form onSubmit={this.handleAddItem}>
+            <input
+              className="new-todo"
+              placeholder="What needs to be done?"
+              value={newItemText}
+              onChange={this.handleChange}
+            />
+          </form>
+        </header>
+
+        <section className="main">
+          <input
+            type="checkbox"
+            id="toggle-all"
+            className="toggle-all"
+            onChange={this.checkAll}
+          />
+          <label htmlFor="toggle-all">Mark all as complete</label>
+          <TodoList
+            todos={filteredTodos}
+            onDelete={this.handleDelete}
+            toggleItem={this.toggleItem}
+            onTextChanged={this.changeText}
+          />
+        </section>
+
+        <footer className="footer">
+          <span className="todo-count">
+            {`${activeTodos.length} items left`}
+          </span>
+
+          <ul className="filters">
+            <li>
+              <a
+                onClick={this.handleOnAll}
+                href="#/"
+                className="selected"
+              >
+                All
+              </a>
+            </li>
+
+            <li>
+              <a
+                onClick={this.handleOnActive}
+                href="#/active"
+              >
+                Active
+              </a>
+            </li>
+
+            <li>
+              <a
+                onClick={this.handleOnCompleted}
+                href="#/completed"
+              >
+                Completed
+              </a>
+            </li>
+          </ul>
+          {todos.some(todo => todo.done === true)
+            && (
+              <button
+                type="button"
+                className="clear-completed"
+                onClick={this.clearCompleted}
+              >
+                Clear completed
+              </button>
+            )}
+        </footer>
       </section>
-
-      <footer className="footer">
-        <span className="todo-count">
-          3 items left
-        </span>
-
-        <ul className="filters">
-          <li>
-            <a href="#/" className="selected">All</a>
-          </li>
-
-          <li>
-            <a href="#/active">Active</a>
-          </li>
-
-          <li>
-            <a href="#/completed">Completed</a>
-          </li>
-        </ul>
-
-        <button type="button" className="clear-completed">
-          Clear completed
-        </button>
-      </footer>
-    </section>
-  );
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Todo/Todo.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import * as cx from 'classnames';
+
+export class Todo extends React.Component {
+  state = {
+    currentValue: '',
+    isEditing: false,
+  }
+
+  startEdit = () => {
+    this.setState({
+      isEditing: true,
+      currentValue: this.props.text,
+    });
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      currentValue: event.target.value,
+    });
+  }
+
+  handleKeyPress = (event, id) => {
+    if (event.key !== 'Enter') {
+      return;
+    }
+
+    this.props.onTextChanged(id, this.state.currentValue);
+
+    this.setState({
+      currentValue: '',
+      isEditing: false,
+    });
+  }
+
+  render() {
+    const { isEditing, currentValue } = this.state;
+    const { id, done, text, toggleItem, onDelete } = this.props;
+
+    return (
+      <li
+        onDoubleClick={this.startEdit}
+        className={cx({
+          completed: done,
+          editing: isEditing,
+        })}
+      >
+        <div className="view">
+          <input
+            type="checkbox"
+            className="toggle"
+            id={`todo-${id}`}
+            checked={done}
+            onChange={() => toggleItem(id)}
+          />
+          <label htmlFor={`todo-${id}`}>{text}</label>
+          <button
+            type="button"
+            className="destroy"
+            onClick={() => onDelete(id)}
+          />
+        </div>
+        {isEditing && (
+          <input
+            type="text"
+            className="edit"
+            value={currentValue}
+            onChange={this.handleChange}
+            onKeyPress={event => this.handleKeyPress(event, id)}
+          />
+        )}
+      </li>
+    );
+  }
+}
+
+Todo.propTypes = {
+  id: PropTypes.string.isRequired,
+  done: PropTypes.bool.isRequired,
+  text: PropTypes.string.isRequired,
+  toggleItem: PropTypes.func.isRequired,
+  onDelete: PropTypes.func.isRequired,
+  onTextChanged: PropTypes.func.isRequired,
+};
