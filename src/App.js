cd7f5b21d056b3828ee865274c7144b5ca9cb5ec
Yarik1993 <makarov1603@icloud.com>
2020-05-08 22:11:54 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ class App extends React.Component {
 
           return todo;
         }),
-        toggleTodosStatus: !state.toggleTodosStatus,
+        toggleTodosStatus: false,
       }
     ));
   };
