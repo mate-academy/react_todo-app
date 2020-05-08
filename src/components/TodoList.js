1e9e13462ef42af0589252b21d4cf063464c9cd8
Alex Machula <alexmachulamail@gmail.com>
2020-05-08 08:52:52 +0300

    solution

---
+++ src/components/TodoList.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const TodoList = ({ todos }) => (
+  <ul className="todo-list">
+    {todos.map(todo => (
+      <li>
+        <div className="view">
+          <input
+            type="checkbox"
+            className="toggle"
+            id={`todo-${todo.id}`}
+          />
+          <label htmlFor={`todo-${todo.id}`}>{todo.title}</label>
+          <button type="button" className="destroy" />
+        </div>
+        <input type="text" className="edit" />
+      </li>
+    ))}
+  </ul>
+);
+
+TodoList.propTypes = {
+  todos: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+  })).isRequired,
+};
+
+export default TodoList;
