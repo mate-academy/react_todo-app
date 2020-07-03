67e4079e7478db30038518b383908c170d0acb80
Liliia <chuiko1112@gmail.com>
2020-07-03 17:35:23 +0300

    conditional rendering for footer

--- src/Footer.js
+++ src/Footer.js
@@ -21,60 +21,73 @@ export const Footer = ({ visibleFooter, selectedButton,
   };
 
   return (
-    <footer
-      className="footer"
-      style={{ display: visibleFooter ? 'block' : 'none' }}
-    >
-      <span className="todo-count">
-        {todoList.length - amountOfCompleted}
-        &nbsp;
-        items left
-      </span>
+    <section>
+      {
+        visibleFooter ? (
+          <footer className="footer">
+            <span className="todo-count">
+              {todoList.length - amountOfCompleted}
+              &nbsp;
+              items left
+            </span>
 
-      <ul className="filters">
-        <li>
-          <a
-            href="#/"
-            className={(selectedButton === 'all' ? 'selected' : 'non-selected')}
-            onClick={showAll}
-          >
-            All
-          </a>
-        </li>
+            <ul className="filters">
+              <li>
+                <a
+                  href="#/"
+                  className={(
+                    selectedButton === 'all' ? 'selected' : 'non-selected'
+                  )}
+                  onClick={showAll}
+                >
+                  All
+                </a>
+              </li>
 
-        <li>
-          <a
-            href="#/active"
-            className={(
-              selectedButton === 'active'
-                ? 'selected'
-                : 'non-selected'
-            )}
-            onClick={ev => showActive(ev)}
-          >
-            Active
-          </a>
-        </li>
+              <li>
+                <a
+                  href="#/active"
+                  className={(
+                    selectedButton === 'active'
+                      ? 'selected'
+                      : 'non-selected'
+                  )}
+                  onClick={ev => showActive(ev)}
+                >
+                  Active
+                </a>
+              </li>
 
-        <li>
-          <a
-            href="#/completed"
-            onClick={showCompleted}
-            className={(
-              selectedButton === 'completed'
-                ? 'selected'
-                : 'non-selected'
-            )}
-          >
-            Completed
-          </a>
-        </li>
-      </ul>
+              <li>
+                <a
+                  href="#/completed"
+                  onClick={showCompleted}
+                  className={(
+                    selectedButton === 'completed'
+                      ? 'selected'
+                      : 'non-selected'
+                  )}
+                >
+                  Completed
+                </a>
+              </li>
+            </ul>
 
-      <button type="button" className="clear-completed" onClick={handleClear}>
-        Clear completed
-      </button>
-    </footer>
+            <button
+              type="button"
+              className="clear-completed"
+              onClick={handleClear}
+            >
+              Clear completed
+            </button>
+          </footer>
+        )
+          : (
+            <div />
+          )
+      }
+    </section>
+
   );
 };
 
