e93e56e41d27e0f6cf551289fd224a8d7c4427a4
Stanislav <stasusss07@gmail.com>
2020-07-12 20:02:13 +0300

    add shapes

--- src/components/Shapes.js
+++ src/components/Shapes.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 
 export const todoShape = PropTypes.shape({
-  id: PropTypes.number,
   title: PropTypes.string,
-  isCompleted: PropTypes.bool,
+  id: PropTypes.number,
+  completed: PropTypes.bool,
 });
