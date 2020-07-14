5b67b209ed4f93ede1d9b878c45bc5bcffecb197
tyooma <flying.alien.godzilla@gmail.com>
2020-07-14 14:12:08 +0300

    TodoApp

--- src/App.js
+++ src/App.js
@@ -1,65 +1,69 @@
 import React from 'react';
+
 import { TodoApp } from './components/TodoApp/TodoApp';
 import { TodoList } from './components/TodoList/TodoList';
-import { TodoFilter } from './components/TodoFilter/TodoFilter';
+import { TodoFilter } from './components/TodoFilter.js/TodoFilter';
 
-export class App extends React.Component {
+class App extends React.Component {
   state = {
     todos: [],
-    show: 'all',
+    showTodos: 'all',
   }
 
-  addTodo = (todo) => {
-    this.setState(prevState => ({
-      todos: [...prevState.todos, todo],
-    }));
+  addTodo = (text, todoId) => {
+    if (text.trim().length) {
+      this.setState(prevState => ({
+        todos: [
+          ...prevState.todos,
+          {
+            id: todoId,
+            todo: text,
+            completed: false,
+          },
+        ],
+      }));
+    }
   }
 
-  checkedTodo = (id) => {
+  checkTodo = (id) => {
     this.setState(prevState => ({
-      todos: prevState.todos.map((todo) => {
-        if (todo.id === id) {
+      todos: prevState.todos.map((item) => {
+        if (item.id === id) {
           return {
-            ...todo,
-            completed: !todo.completed,
+            ...item,
+            completed: !item.completed,
           };
         }
 
-        return todo;
-      }),
-    }));
-  }
-
-  editTodo = (edited, id) => {
-    this.setState(prevState => ({
-      todos: prevState.todos.map((todo) => {
-        if (todo.id === id) {
-          return {
-            ...todo,
-            title: edited,
-          };
-        }
-
-        return todo;
+        return item;
       }),
     }));
   }
 
   removeTodo = (id) => {
     this.setState(prevState => ({
-      todos: prevState.todos.filter(todo => todo.id !== id),
+      todos: prevState.todos.filter(item => item.id !== id),
     }));
   }
 
-  makeThemChecked = () => {
+  editTodo = (editedTodo, id) => {
     this.setState(prevState => ({
-      todos: prevState.todos.map(todo => todo.completed === 'true'),
+      todos: prevState.todos.map((item) => {
+        if (item.id === id) {
+          return {
+            ...item,
+            todo: editedTodo,
+          };
+        }
+
+        return item;
+      }),
     }));
   }
 
-  filterChange = (filter) => {
+  filterTodos = (filter) => {
     this.setState({
-      show: filter,
+      showTodos: filter,
     });
   }
 
@@ -69,31 +73,46 @@ export class App extends React.Component {
     }));
   }
 
-  show(selectedFilter) {
-    if (selectedFilter === 'all') {
+  makeThemChecked = () => {
+    if (this.state.todos.every(todo => todo.completed === true)) {
+      this.setState(prevState => ({
+        todos: prevState.todos.map(todo => ({
+          ...todo,
+          completed: false,
+        })),
+      }));
+    } else {
+      this.setState(prevState => ({
+        todos: prevState.todos.map(todo => ({
+          ...todo,
+          completed: true,
+        })),
+      }));
+    }
+  }
+
+  showTodos(shownTodos) {
+    if (shownTodos === 'all') {
       return this.state.todos;
     }
 
-    if (selectedFilter === 'active') {
-      return this.state.todos.filter(todo => !todo.completed);
+    if (shownTodos === 'active') {
+      return this.state.todos.filter(todo => todo.completed === false);
     }
 
-    if (selectedFilter === 'completed') {
-      return this.state.todos.filter(todo => todo.completed);
+    if (shownTodos === 'completed') {
+      return this.state.todos.filter(todo => todo.completed === true);
     }
 
-    return '';
+    return null;
   }
 
   render() {
-    const { todos, show } = this.state;
+    const { todos } = this.state;
 
     return (
       <section className="todoapp">
-        <TodoApp
-          todos={todos}
-          addTodo={this.addTodo}
-        />
+        <TodoApp addTodo={this.addTodo} />
         <section className="main">
           <input
             type="checkbox"
@@ -102,25 +121,31 @@ export class App extends React.Component {
             onClick={this.makeThemChecked}
           />
           <label htmlFor="toggle-all">Mark all as complete</label>
-          <TodoList
-            todos={this.show(show)}
-            checked={this.checkedTodo}
-            edit={this.editTodo}
-            remove={this.removeTodo}
-          />
-        </section>
-        {
-          todos.length
+          {todos.length
             ? (
-              <TodoFilter
-                todos={todos}
-                filterChange={this.filterChange}
-                clearCompleted={this.clearCompleted}
+              <TodoList
+                todos={this.showTodos(this.state.showTodos)}
+                check={this.checkTodo}
+                remove={this.removeTodo}
+                edit={this.editTodo}
               />
             )
-            : ''
+            : <></>
+          }
+        </section>
+        {todos.length
+          ? (
+            <TodoFilter
+              todos={this.state.todos}
+              filterTodos={this.filterTodos}
+              clear={this.clearCompleted}
+            />
+          )
+          : <></>
         }
       </section>
     );
   }
 }
+
+export default App;
