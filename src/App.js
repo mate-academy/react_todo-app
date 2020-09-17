d261742966d623e390e8eba8485dd6e142ff02b9
oggyrock <steelo.sun@gmail.com>
2020-09-17 21:48:37 +0300

    splitted into components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import { TodoList } from './components/TodoList';
+import { TodosFilter } from './components/TodosFilter';
 
 function App() {
   const [title, setTitle] = useState('');
@@ -17,6 +19,7 @@ function App() {
 
     return true;
   });
+
   const newTodo = {
     id: +new Date(),
     title,
@@ -79,125 +82,20 @@ function App() {
         >
           Mark all as complete
         </label>
-
-        <ul className="todo-list">
-          {filteredList.map((todo, index) => (
-            <li
-              hidden={todo.hidden}
-              key={todo.id}
-              className={todo.completed ? 'completed' : undefined}
-              onDoubleClick={(event) => {
-                const clickedTodo = event.target;
-
-                setUneditedTitles({
-                  ...uneditedTitles,
-                  [todo.id]: todo.title,
-                });
-                clickedTodo.closest('li').className = 'editing';
-              }}
-            >
-              <div className="view">
-                <input
-                  type="checkbox"
-                  className="toggle"
-                  checked={todo.completed}
-                  onChange={() => {
-                    todoList[index].completed = !todo.completed;
-                    setTodoList([...todoList]);
-                  }}
-                />
-                <label>{todo.title}</label>
-                <button
-                  type="button"
-                  className="destroy"
-                  onClick={() => {
-                    todoList.splice(index, 1);
-                    setTodoList([...todoList]);
-                  }}
-                />
-              </div>
-              <input
-                type="text"
-                className="edit"
-                value={todo.title}
-                onChange={(event) => {
-                  todoList[index].title = event.target.value;
-                  setTodoList([...todoList]);
-                }}
-                onKeyDown={(event) => {
-                  const clickedTodo = event.target;
-
-                  if (event.key === 'Enter') {
-                    clickedTodo.closest('li').className
-                      = todo.completed ? 'completed' : '';
-                  }
-
-                  if (event.key === 'Escape') {
-                    todoList[index].title = uneditedTitles[todo.id];
-                    setTodoList([...todoList]);
-                    clickedTodo.closest('li').className
-                      = todo.completed ? 'completed' : '';
-                  }
-                }}
-              />
-            </li>
-          ))}
-        </ul>
+        <TodoList
+          filteredList={filteredList}
+          setUneditedTitles={setUneditedTitles}
+          uneditedTitles={uneditedTitles}
+          todoList={todoList}
+          setTodoList={setTodoList}
+        />
       </section>
-
-      <footer
-        className="footer"
-        hidden={!todoList.length}
-      >
-        <span className="todo-count">
-          {todoList.filter(todo => !todo.completed).length}
-          {' '}
-          items left
-        </span>
-
-        <ul className="filters">
-          <li>
-            <a
-              href="#/"
-              className={filter === 'All' ? 'selected' : ''}
-              onClick={() => setFilter('All')}
-            >
-              All
-            </a>
-          </li>
-
-          <li>
-            <a
-              href="#/active"
-              className={filter === 'Active' ? 'selected' : ''}
-              onClick={() => setFilter('Active')}
-            >
-              Active
-            </a>
-          </li>
-
-          <li>
-            <a
-              href="#/completed"
-              className={filter === 'Completed' ? 'selected' : ''}
-              onClick={() => setFilter('Completed')}
-            >
-              Completed
-            </a>
-          </li>
-        </ul>
-
-        <button
-          type="button"
-          hidden={todoList.every(todo => !todo.completed)}
-          className="clear-completed"
-          onClick={() => setTodoList(
-            [...todoList.filter(todo => !todo.completed)],
-          )}
-        >
-          Clear completed
-        </button>
-      </footer>
+      <TodosFilter
+        todoList={todoList}
+        setTodoList={setTodoList}
+        filter={filter}
+        setFilter={setFilter}
+      />
     </section>
   );
 }
