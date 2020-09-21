8d1ef3a1f62fc2accb1b7b9f370701e4b62f836a
sobchuksasha <sobchuksasha1995@gmail.com>
2020-09-21 22:47:45 +0300

    version

--- src/App.js
+++ src/App.js
@@ -21,8 +21,17 @@ function App() {
   return (
     <section className="todoapp">
       <header className="header">
+
         <h1>todos</h1>
         <AddingForm setTodos={setTodos} />
+
+        <h1>todos App</h1>
+
+        <input
+          className="new-todo"
+          placeholder="What needs to be done?"
+          autoFocus=""
+        />
       </header>
 
       <section className="main">
