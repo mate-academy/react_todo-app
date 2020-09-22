186bb60d772e873408820576b4d10b777dff004b
mpone <m.parphenjuk@gmail.com>
2020-09-22 12:44:56 +0300

    1-9

---
+++ src/api/todos.js
@@ -0,0 +1,29 @@
+export const todosList = [
+  {
+    id: 1,
+    title: 'first',
+    completed: false,
+  },
+  {
+    id: 2,
+    title: 'second',
+    completed: false,
+  },
+  {
+    id: 3,
+    title: 'third',
+    completed: false,
+  },
+];
+
+// export const removeTodo = (todoId) => {
+//   todosList = todosList.filter(todo => todo.id !== todoId);
+// };
+
+// export const addTodo = (title) => {
+//   todosList.push({
+//     id: new Date(),
+//     title,
+//     completed: false,
+//   });
+// };
