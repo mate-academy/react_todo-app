8bdacc92cffd06a42cc4a6e918240abf718eefef
38063 <xeilen1112@gmail.com>
2020-11-16 20:46:52 +0200

    -implement 1-4 and 11 (16.11.2020)

--- src/components/NewTodo/NewTodoComponent.js
+++ src/components/NewTodo/NewTodoComponent.js
@@ -1,12 +1,32 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
-export const NewTodoComponent = () => {
+export const NewTodoComponent = ({ setTodos }) => {
   const [title, setTitle] = useState('');
 
   const handleChange = (e) => {
     const { value } = e.target;
 
     setTitle(value);
+  };
+
+  const newTodo = (e) => {
+    const todo = {
+      id: Date.now(),
+      title,
+      completed: false,
+    };
+
+    if (e.key === 'Enter') {
+      e.preventDefault();
+
+      if (!title) {
+        return;
+      }
+
+      setTodos(prevState => [...prevState, todo]);
+      setTitle('');
+    }
   };
 
   return (
@@ -17,7 +37,12 @@ export const NewTodoComponent = () => {
         placeholder="What needs to be done?"
         value={title}
         onChange={handleChange}
+        onKeyDown={newTodo}
       />
     </form>
   );
+};
+
+NewTodoComponent.propTypes = {
+  setTodos: PropTypes.func.isRequired,
 };
---
+++ src/components/TodoList/TodoList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { TodoItem } from '../TodoItem/TodoItem';
+
+export const TodoList = ({ todos, onChangeStatus }) => (
+  <ul className="todo-list">
+    {todos.map(todo => (
+      <TodoItem key={todo.id} todo={todo} onChangeStatus={onChangeStatus} />
+    ))}
+  </ul>
+);
+
+TodoList.propTypes = {
+  todos: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    completed: PropTypes.bool.isRequired,
+  })).isRequired,
+  onChangeStatus: PropTypes.func.isRequired,
+};
