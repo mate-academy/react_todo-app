754aac67bca6f35c54366abc4f554f7014454408
DarMatt <daryamatviienko@gmail.com>
2021-02-03 22:06:16 +0200

    Solution

--- src/App.js
+++ src/App.js
@@ -1,86 +1,183 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [newTodoTitle, setNewTodo] = useState('');
+  const [status, setStatus] = useState(false);
+  // const [selectedFilter, setFilter] = useState('All');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setTodos(prevTodos => (
+      [...prevTodos,
+        {
+          id: +new Date(),
+          title: newTodoTitle,
+          completed: false,
+        },
+      ]
+    ));
+    setNewTodo('');
+  };
+
+  const deleteTodo = (todoId) => {
+    setTodos(todos.filter(todo => todo.id !== todoId));
+  };
+
+  const toggleCompletedStatus = (todoId) => {
+    setTodos(todos.map((todo) => {
+      if (todoId === todo.id) {
+        return {
+          ...todo,
+          completed: !todo.completed,
+        };
+      }
+
+      return todo;
+    }));
+  };
+
+  const makeAllTodosComplete = () => {
+    if (status) {
+      setTodos(todos.map(todo => ({
+        ...todo,
+        completed: false,
+      })));
+    } else {
+      setTodos(todos.map(todo => ({
+        ...todo,
+        completed: true,
+      })));
+    }
+
+    setStatus(!status);
+  };
+
+  const uncompletedTodos = todos.filter(todo => !todo.completed);
+  const clearCompleted = () => {
+    setTodos(uncompletedTodos);
+  };
+
+  const completedTodos = todos.filter(todo => todo.completed);
+
+  const filterTodos = (selectedFilter) => {
+    const FILTERS = {
+      All: todos,
+      Active: todos.filter(todo => !todo.completed),
+      Completed: todos.filter(todo => todo.completed),
+    };
+
+    setTodos(FILTERS[selectedFilter]);
+  };
+
+  // const changeFilter = (event) => {
+  //   event.preventDefault();
+  //   setFilter(event.target.name);
+  // };
+
   return (
     <section className="todoapp">
       <header className="header">
         <h1>todos</h1>
 
-        <form>
+        <form
+          onSubmit={handleSubmit}
+        >
           <input
             type="text"
             className="new-todo"
             placeholder="What needs to be done?"
+            value={newTodoTitle}
+            onChange={event => setNewTodo(event.target.value)}
           />
         </form>
       </header>
 
       <section className="main">
-        <input type="checkbox" id="toggle-all" className="toggle-all" />
+        <input
+          type="checkbox"
+          id="toggle-all"
+          className="toggle-all"
+          onChange={makeAllTodosComplete}
+        />
+
         <label htmlFor="toggle-all">Mark all as complete</label>
 
         <ul className="todo-list">
-          <li>
-            <div className="view">
-              <input type="checkbox" className="toggle" />
-              <label>asdfghj</label>
-              <button type="button" className="destroy" />
-            </div>
-            <input type="text" className="edit" />
-          </li>
+          {todos.map(todo => (
+            <li key={todo.id}>
+              <div className="view">
+                <input
+                  type="checkbox"
+                  className="toggle"
+                  onClick={() => toggleCompletedStatus(todo.id)}
+                  checked={todo.completed}
+                />
+                <label>{todo.title}</label>
+                <button
+                  type="button"
+                  className="destroy"
+                  onClick={() => deleteTodo(todo.id)}
+                />
+              </div>
+              <input type="text" className="edit" />
+            </li>
+          ))}
 
-          <li className="completed">
-            <div className="view">
-              <input type="checkbox" className="toggle" />
-              <label>qwertyuio</label>
-              <button type="button" className="destroy" />
-            </div>
-            <input type="text" className="edit" />
-          </li>
-
-          <li className="editing">
-            <div className="view">
-              <input type="checkbox" className="toggle" />
-              <label>zxcvbnm</label>
-              <button type="button" className="destroy" />
-            </div>
-            <input type="text" className="edit" />
-          </li>
-
-          <li>
-            <div className="view">
-              <input type="checkbox" className="toggle" />
-              <label>1234567890</label>
-              <button type="button" className="destroy" />
-            </div>
-            <input type="text" className="edit" />
-          </li>
         </ul>
       </section>
+      {todos.length > 0 && (
+        <footer className="footer">
+          <span className="todo-count">
+            {uncompletedTodos.length}
+            {' '}
+            items left
+          </span>
 
-      <footer className="footer">
-        <span className="todo-count">
-          3 items left
-        </span>
+          <ul className="filters">
+            <li>
+              <a
+                href="#/"
+                className="selected"
+                name="All"
+                onClick={() => filterTodos('All')}
+              >
+                All
+              </a>
+            </li>
 
-        <ul className="filters">
-          <li>
-            <a href="#/" className="selected">All</a>
-          </li>
+            <li>
+              <a
+                href="#/active"
+                name="Active"
+                onClick={() => filterTodos('Active')}
+              >
+                Active
+              </a>
+            </li>
 
-          <li>
-            <a href="#/active">Active</a>
-          </li>
+            <li>
+              <a
+                href="#/completed"
+                name="Completed"
+                onClick={() => filterTodos('Completed')}
+              >
+                Completed
+              </a>
+            </li>
+          </ul>
 
-          <li>
-            <a href="#/completed">Completed</a>
-          </li>
-        </ul>
-
-        <button type="button" className="clear-completed">
-          Clear completed
-        </button>
-      </footer>
+          {completedTodos.length > 0 && (
+            <button
+              type="button"
+              className="clear-completed"
+              onClick={clearCompleted}
+            >
+              Clear completed
+            </button>
+          )}
+        </footer>
+      )}
     </section>
   );
 }
