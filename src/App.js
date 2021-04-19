7e5ce87bcae7729401ed97fe8fb0d9a7530ced26
Smihotur Roman <smikhotur@gmail.com>
2021-04-19 18:14:48 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLocation } from 'react-router';
-import { useLocalStorage } from './useLocalStorage';
+// import { useLocalStorage } from './useLocalStorage';
 import { Header } from './components/Header';
 import { TodoList } from './components/TodoList';
 import { Footer } from './components/Footer';
 
 export const App = () => {
-  const [todos, setTodos] = useLocalStorage([]);
+  const [todos, setTodos] = useState([]);
 
   const setStatus = (todoId) => {
     setTodos(prevTodos => prevTodos.map((todo) => {
