22dd203f83fe7724487739cb0c4ed50842068821
Vladyslav Polets <46418908+vpolets@users.noreply.github.com>
2021-09-28 13:49:25 +0300

    Update repo. Added cypress (#334)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,4 @@
 module.exports = {
-  extends: '@mate-academy/eslint-config-react',
-  rules: {
-    "jsx-a11y/no-autofocus": 0,
-    "jsx-a11y/label-has-associated-control": 0,
-    "object-curly-newline": [2, {
-      "ObjectExpression": {
-        "consistent": true,
-        "minProperties": 4,
-      },
-    }],
-  },
+  extends: ['@mate-academy/eslint-config-react-typescript', 'plugin:cypress/recommended'],
+  // extends: '@mate-academy/eslint-config-react',
 };
