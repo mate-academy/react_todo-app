b9508b666a78d867b3c72b035e9cec413655c314
Victor Shevchuk <callmevitok@gmail.com>
2021-12-06 12:34:49 +0200

    1

---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,39 @@
+/* eslint-disable react/prop-types */
+import React, { useState } from 'react';
+
+export const TodoForm = ({ updateTodos }) => {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInput = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const hanbleSubmit = (event) => {
+    event.preventDefault();
+
+    if (inputValue === '') {
+      return;
+    }
+
+    const createTodo = {
+      id: +new Date(),
+      title: inputValue,
+      completed: false,
+    };
+
+    updateTodos(createTodo);
+    setInputValue('');
+  };
+
+  return (
+    <form onSubmit={hanbleSubmit}>
+      <input
+        type="text"
+        value={inputValue}
+        className="new-todo"
+        placeholder="What needs to be done?"
+        onChange={handleInput}
+      />
+    </form>
+  );
+};
