ec26872e49bb732551173e8565376e08d364ec90
SpaceHook <maxdumiak39@gmail.com>
2022-08-04 22:43:53 +0300

    add dot

--- src/components/TodoApp/TodoApp.tsx
+++ src/components/TodoApp/TodoApp.tsx
@@ -111,7 +111,7 @@ export const TodoApp: React.FC = () => {
   return (
     <div className="todoapp">
       <header className="header">
-        <h1>todos</h1>
+        <h1>todos.</h1>
       </header>
 
       <form onSubmit={onSubmit}>
