e90de81cf98311d0e0a8a0126c59b30219e0128f
Illia Bielichenko <ibelich12@gmail.com>
2022-08-12 14:19:23 +0300

    version 1 was added

--- src/App.tsx
+++ src/App.tsx
@@ -1,93 +1,133 @@
+/* eslinewTodoTitlent-disable @typescript-eslint/indent */
+/* eslint-disable no-console */
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
+import React, { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import { TodoList } from './components/TodoList';
+import { Footer } from './components/Footer';
+import { Todo } from './types/todo';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>(JSON.parse(`${localStorage.getItem('todos')}`) || []);
+  const [newTodoTitle, setNewTodoTitle] = useState<string>('');
+
+  const makeTodoTitle = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewTodoTitle(event.target.value);
+  };
+
+  const makeTodo = (e: React.FormEvent) => {
+    e.preventDefault();
+    const validTodoTitle = newTodoTitle.trim();
+
+    if (validTodoTitle) {
+      const newTodoObj = {
+        id: Math.random().toString(36).slice(2, 7),
+        title: validTodoTitle,
+        completed: false,
+      };
+
+      setTodos([...todos, newTodoObj]);
+      localStorage.setItem('todos', JSON.stringify([...todos, newTodoObj]));
+      setNewTodoTitle('');
+    }
+  };
+
+  const deleteTodo = (id: string) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+
+    setTodos(updatedTodos);
+    localStorage.setItem('todos', JSON.stringify(updatedTodos));
+  };
+
+  const toggleTodoStatus = (id: string) => {
+    let updatedTodos;
+
+    if (id === 'toggleAll') {
+      if (todos.some(todo => todo.completed === false)) {
+        updatedTodos = todos.map(
+          todo => ({ ...todo, completed: true }),
+        );
+      } else {
+        updatedTodos = todos.map(
+          todo => ({ ...todo, completed: false }),
+        );
+      }
+    } else {
+      updatedTodos = todos.map(todo => (todo.id === id
+        ? { ...todo, completed: !todo.completed }
+        : todo));
+    }
+
+    setTodos(updatedTodos);
+    localStorage.setItem('todos', JSON.stringify(updatedTodos));
+  };
+
   return (
     <div className="todoapp">
       <header className="header">
         <h1>todos</h1>
 
-        <form>
+        <form onSubmit={(e) => makeTodo(e)}>
           <input
             type="text"
             data-cy="createTodo"
             className="new-todo"
             placeholder="What needs to be done?"
+            onChange={(event) => makeTodoTitle(event)}
+            value={newTodoTitle}
           />
         </form>
       </header>
 
       <section className="main">
-        <input
-          type="checkbox"
-          id="toggle-all"
-          className="toggle-all"
-          data-cy="toggleAll"
-        />
-        <label htmlFor="toggle-all">Mark all as complete</label>
+        {todos[0] && (
+          <input
+            type="checkbox"
+            id="toggle-all"
+            className="toggle-all"
+            data-cy="toggleAll"
+            onChange={() => toggleTodoStatus('toggleAll')}
+          />
+        )}
+        <label htmlFor="toggle-all"> </label>
+        <Routes>
+          <Route
+            path="/"
+            element={(
+              <TodoList
+                todos={todos}
+                deleteTodo={deleteTodo}
+                toggleTodoStatus={toggleTodoStatus}
+              />
+            )}
+          />
+          <Route
+            path="active"
+            element={(
+              <TodoList
+                todos={todos.filter(todo => todo.completed === false)}
+                deleteTodo={deleteTodo}
+                toggleTodoStatus={toggleTodoStatus}
+              />
+            )}
+          />
+          <Route
+            path="completed"
+            element={(
+              <TodoList
+                todos={todos.filter(todo => todo.completed === true)}
+                deleteTodo={deleteTodo}
+                toggleTodoStatus={toggleTodoStatus}
+              />
+            )}
+          />
 
-        <ul className="todo-list" data-cy="todoList">
-          <li>
-            <div className="view">
-              <input type="checkbox" className="toggle" id="toggle-view" />
-              <label htmlFor="toggle-view">asdfghj</label>
-              <button type="button" className="destroy" data-cy="deleteTodo" />
-            </div>
-            <input type="text" className="edit" />
-          </li>
-
-          <li className="completed">
-            <div className="view">
-              <input type="checkbox" className="toggle" id="toggle-completed" />
-              <label htmlFor="toggle-completed">qwertyuio</label>
-              <button type="button" className="destroy" data-cy="deleteTodo" />
-            </div>
-            <input type="text" className="edit" />
-          </li>
-
-          <li className="editing">
-            <div className="view">
-              <input type="checkbox" className="toggle" id="toggle-editing" />
-              <label htmlFor="toggle-editing">zxcvbnm</label>
-              <button type="button" className="destroy" data-cy="deleteTodo" />
-            </div>
-            <input type="text" className="edit" />
-          </li>
-
-          <li>
-            <div className="view">
-              <input type="checkbox" className="toggle" id="toggle-view2" />
-              <label htmlFor="toggle-view2">1234567890</label>
-              <button type="button" className="destroy" data-cy="deleteTodo" />
-            </div>
-            <input type="text" className="edit" />
-          </li>
-        </ul>
+        </Routes>
       </section>
-
-      <footer className="footer">
-        <span className="todo-count" data-cy="todosCounter">
-          3 items left
-        </span>
-
-        <ul className="filters">
-          <li>
-            <a href="#/" className="selected">All</a>
-          </li>
-
-          <li>
-            <a href="#/active">Active</a>
-          </li>
-
-          <li>
-            <a href="#/completed">Completed</a>
-          </li>
-        </ul>
-
-        <button type="button" className="clear-completed">
-          Clear completed
-        </button>
-      </footer>
+      {todos.length
+        ? (<Footer todos={todos} setTodos={setTodos} />)
+        : ''}
     </div>
   );
 };
