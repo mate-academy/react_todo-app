a651a898ba1aaacfeb770d6f33aff31fa5738016
Kris Mur <budkevych.h@gmail.com>
2022-08-13 15:56:08 +0200

    rewrite solution

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import ReactDOM from 'react-dom';
-
 import './styles/index.css';
 import './styles/todo-list.css';
 import './styles/filters.css';
+import { HashRouter } from 'react-router-dom';
 
 import { App } from './App';
 
 ReactDOM.render(
-  <App />,
+  <HashRouter>
+    <App />
+  </HashRouter>,
   document.getElementById('root'),
 );
