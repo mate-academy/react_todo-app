247a4d884b2e04c5f2d43369812065f33f056833
Viacheslav Tsymbal <slava.tsymbal@ukr.net>
2022-08-15 11:41:11 +0300

    fixes after mentor`s review

--- src/App.tsx
+++ src/App.tsx
@@ -1,171 +1,25 @@
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React, { useState } from 'react';
+
 import { Route, Routes } from 'react-router-dom';
 import { TodoApp } from './components/TodoApp';
-import { TodoFilter } from './components/TodoFilter';
-import { TodoList } from './components/TodoList';
-import { Todo } from './types/Todo';
-
-const useLocalStorage = (key: string, initialValue: Todo[]) => {
-  const [value, setValue] = useState(() => {
-    try {
-      return JSON.parse(localStorage.getItem(key) || '') || initialValue;
-    } catch {
-      return initialValue;
-    }
-  });
-
-  const save = (value1: Todo[]) => {
-    setValue(value1);
-    localStorage.setItem(key, JSON.stringify(value1));
-  };
-
-  return [value, save];
-};
 
 export const App: React.FC = () => {
-  const [todos, setTodos] = useLocalStorage('todos', []);
-  const [completed, setCompleted] = useState(true);
-
-  const addTodo = (title: string) => {
-    const newTodo: Todo = {
-      id: Math.random(),
-      title: title.trim(),
-      completed: false,
-    };
-
-    setTodos([...todos, newTodo]);
-  };
-
-  const deleteTodo = (todoId: number) => {
-    setTodos(todos.filter((todo: Todo) => todo.id !== todoId));
-  };
-
-  const editTodo = (todoId: number, todoTitle: string) => {
-    setTodos(todos.map((todo: Todo) => {
-      if (todo.id !== todoId) {
-        return todo;
-      }
-
-      return {
-        ...todo,
-        title: todoTitle,
-      };
-    }));
-  };
-
-  const editTodoStatus = (todoId: number, todoStatus: boolean) => {
-    setTodos(todos.map((todo: Todo) => {
-      if (todo.id !== todoId) {
-        return todo;
-      }
-
-      return {
-        ...todo,
-        completed: !todoStatus,
-      };
-    }));
-  };
-
-  const clearAllCompleted = () => {
-    setTodos(todos
-      .filter((todo: Todo) => todo.completed !== true));
-  };
-
-  const changeAllCompleted = () => {
-    setTodos(todos.map((todo: Todo) => {
-      return {
-        ...todo,
-        completed,
-      };
-    }));
-  };
-
-  const todosLeft = todos
-    .filter((todo: Todo) => todo.completed === false);
-  const completedTodo = todos
-    .some((todo: Todo) => todo.completed === true);
-
   return (
-    <div className="todoapp">
-      <header className="header">
-        <h1>todos</h1>
-        <TodoApp addTodo={addTodo} />
-      </header>
+    <Routes>
+      <Route
+        path="/"
+        element={<TodoApp />}
+      />
 
-      {todos.length > 0 && (
-        <>
-          <section className="main">
-            <input
-              type="checkbox"
-              id="toggle-all"
-              className="toggle-all"
-              data-cy="toggleAll"
-              onClick={() => {
-                changeAllCompleted();
-                setCompleted(!completed);
-              }}
-            />
-            <label htmlFor="toggle-all">Mark all as complete</label>
+      <Route
+        path="/active"
+        element={<TodoApp />}
+      />
 
-            <Routes>
-              <Route
-                path="/"
-                element={(
-                  <TodoList
-                    todos={todos}
-                    onDelete={deleteTodo}
-                    onEdit={editTodo}
-                    onChangeStatus={editTodoStatus}
-                  />
-                )}
-              />
-
-              <Route
-                path="/active"
-                element={(
-                  <TodoList
-                    todos={todos
-                      .filter((todo: Todo) => todo.completed === false)}
-                    onDelete={deleteTodo}
-                    onEdit={editTodo}
-                    onChangeStatus={editTodoStatus}
-                  />
-                )}
-              />
-
-              <Route
-                path="/completed"
-                element={(
-                  <TodoList
-                    todos={todos
-                      .filter((todo: Todo) => todo.completed === true)}
-                    onDelete={deleteTodo}
-                    onEdit={editTodo}
-                    onChangeStatus={editTodoStatus}
-                  />
-                )}
-              />
-            </Routes>
-          </section>
-
-          <footer className="footer">
-            <span className="todo-count" data-cy="todosCounter">
-              {`${todosLeft.length} items left`}
-            </span>
-            <TodoFilter />
-            {completedTodo && (
-              <button
-                type="button"
-                className="clear-completed"
-                onClick={clearAllCompleted}
-              >
-                Clear completed
-              </button>
-            )}
-          </footer>
-        </>
-      )}
-    </div>
+      <Route
+        path="/completed"
+        element={<TodoApp />}
+      />
+    </Routes>
   );
 };
--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -1,11 +1,105 @@
 import { FC, FormEvent, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import { Todo } from '../types/Todo';
+import { TodoFilter } from './TodoFilter';
+import { TodoList } from './TodoList';
 
-type Props = {
-  addTodo: (title: string) => void
+const useLocalStorage = (key: string, initialValue: Todo[]) => {
+  const [value, setValue] = useState(() => {
+    try {
+      return JSON.parse(localStorage.getItem(key) || '') || initialValue;
+    } catch {
+      return initialValue;
+    }
+  });
+
+  const save = (value1: Todo[]) => {
+    setValue(value1);
+    localStorage.setItem(key, JSON.stringify(value1));
+  };
+
+  return [value, save];
 };
 
-export const TodoApp: FC<Props> = ({ addTodo }) => {
+export const TodoApp: FC = () => {
+  const [todos, setTodos] = useLocalStorage('todos', []);
   const [title, setTitle] = useState('');
+  const [completed, setCompleted] = useState(true);
+  const path = useLocation();
+
+  let visibleTodos: Todo[] = todos;
+
+  switch (path.pathname) {
+    case '/active':
+      visibleTodos = visibleTodos.filter(todo => !todo.completed);
+      break;
+
+    case '/completed':
+      visibleTodos = visibleTodos.filter(todo => todo.completed);
+      break;
+
+    default:
+      break;
+  }
+
+  const addTodo = (newTitle: string) => {
+    const newTodo: Todo = {
+      id: +new Date(),
+      title: newTitle.trim(),
+      completed: false,
+    };
+
+    setTodos([...todos, newTodo]);
+  };
+
+  const deleteTodo = (todoId: number) => {
+    setTodos(todos.filter((todo: Todo) => todo.id !== todoId));
+  };
+
+  const editTodo = (todoId: number, todoTitle: string) => {
+    setTodos(todos.map((todo: Todo) => {
+      if (todo.id !== todoId) {
+        return todo;
+      }
+
+      return {
+        ...todo,
+        title: todoTitle,
+      };
+    }));
+  };
+
+  const editTodoStatus = (todoId: number, todoStatus: boolean) => {
+    setTodos(todos.map((todo: Todo) => {
+      if (todo.id !== todoId) {
+        return todo;
+      }
+
+      return {
+        ...todo,
+        completed: !todoStatus,
+      };
+    }));
+  };
+
+  const clearAllCompleted = () => {
+    setTodos(todos
+      .filter((todo: Todo) => todo.completed !== true));
+  };
+
+  const changeAllCompleted = () => {
+    setTodos(todos.map((todo: Todo) => {
+      return {
+        ...todo,
+        completed,
+      };
+    }));
+  };
+
+  const todosLeft = todos
+    .filter((todo: Todo) => !todo.completed);
+  const completedTodo = todos
+    .some((todo: Todo) => todo.completed);
   const onSubmit = (event: FormEvent) => {
     event.preventDefault();
 
@@ -20,16 +114,61 @@ export const TodoApp: FC<Props> = ({ addTodo }) => {
   };
 
   return (
-    <form onSubmit={onSubmit}>
-      <input
-        type="text"
-        data-cy="createTodo"
-        className="new-todo"
-        placeholder="What needs to be done?"
-        value={title}
-        onChange={(event) => setTitle(event.target.value)}
-        onBlur={onSubmit}
-      />
-    </form>
+    <div className="todoapp">
+      <header className="header">
+        <h1>todos</h1>
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            data-cy="createTodo"
+            className="new-todo"
+            placeholder="What needs to be done?"
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}
+            onBlur={onSubmit}
+          />
+        </form>
+      </header>
+
+      {todos.length > 0 && (
+        <>
+          <section className="main">
+            <input
+              type="checkbox"
+              id="toggle-all"
+              className="toggle-all"
+              data-cy="toggleAll"
+              onClick={() => {
+                changeAllCompleted();
+                setCompleted(!completed);
+              }}
+            />
+            <label htmlFor="toggle-all">Mark all as complete</label>
+            <TodoList
+              todos={visibleTodos}
+              onDelete={deleteTodo}
+              onEdit={editTodo}
+              onChangeStatus={editTodoStatus}
+            />
+          </section>
+
+          <footer className="footer">
+            <span className="todo-count" data-cy="todosCounter">
+              {`${todosLeft.length} items left`}
+            </span>
+            <TodoFilter />
+            {completedTodo && (
+              <button
+                type="button"
+                className="clear-completed"
+                onClick={clearAllCompleted}
+              >
+                Clear completed
+              </button>
+            )}
+          </footer>
+        </>
+      )}
+    </div>
   );
 };
