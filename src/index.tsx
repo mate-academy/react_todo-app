837267734ef1feee9cd0662d93cab418ea7b66ea
Ksenia Didukh <k.tsviatko@gmail.com>
2022-09-12 23:30:46 +0300

    Solution

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,28 @@
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import './styles/index.css';
 import './styles/todo-list.css';
 import './styles/filters.css';
+// import './styles/todo-items.css';
 
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import { App } from './App';
+import { Todos } from './components/Todos';
+import { TodosProvider } from './components/TodosContext';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root'),
+createRoot(document.getElementById('root') as HTMLDivElement).render(
+  <TodosProvider>
+    <HashRouter>
+      <Routes>
+        <Route path="*" element={<h1 className="title">Page not found</h1>} />
+        <Route path="/" element={<App />}>
+          <Route path="*" element={<h1 className="title">Page not found</h1>} />
+
+          <Route index element={<Todos />} />
+          <Route path=":filter" element={<Todos />} />
+        </Route>
+      </Routes>
+    </HashRouter>
+  </TodosProvider>,
+
 );
