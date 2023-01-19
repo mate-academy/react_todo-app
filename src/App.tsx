513977fc81e6f663c85a4cc6801b2fd3faf81127
Vladyslava Buzova <dev.vladyslava@gmail.com>
2023-01-19 21:10:58 +0800

    last version

--- src/App.tsx
+++ src/App.tsx
@@ -5,29 +5,11 @@ import { Footer } from './components/footer';
 import { Header } from './components/header';
 import { Main } from './components/main';
 import { Todo } from './types/Todo';
+import { useLocalStorage } from './components/localstorage';
 
 export const App: React.FC = () => {
   const [input, setInput] = useState('');
-  // const [data, setData] = useState<Todo[]>([]);
-
-  const useLocalStorage = (key:string, initialValue: any) => {
-    const firstValue = localStorage.getItem(key);
-    const jsonValue = firstValue !== null
-      ? JSON.parse(firstValue)
-      : initialValue;
-    const [data, setData] = useState(jsonValue);
-    // JSON.parse(firstValue) || initialValue,
-
-    const save = (value: any) => {
-      setData(value);
-      localStorage.setItem('data', JSON.stringify(value));
-    };
-
-    return [data, save];
-  };
-
   const [data, setData] = useLocalStorage('data', []);
-
   const [filterTodos, setFilterTodos] = useState<Todo[]>(data);
 
   useEffect(() => {
