95e0363c5f3c7448d349b988a28b0634755f998d
Novyk Anton <novyk.anton@gmail.com>
2023-03-31 14:26:17 +0300

    Replace with template string

--- src/UserWarning.tsx
+++ src/UserWarning.tsx
@@ -3,19 +3,16 @@ import React from 'react';
 export const UserWarning: React.FC = () => (
   <section className="section">
     <p className="box is-size-3">
-
-      Please get your
+      {`Please get your `}
 
       <b>
-        {' '}
-        {'userId '}
-        {' '}
+        {`userId `}
       </b>
       <a href="https://mate-academy.github.io/react_student-registration">
         here
       </a>
-      {' and save it in the app '}
-      <pre>const USER_ID = ...</pre>
+      {` and save it in the app `}
+      <pre>USER_ID = ...</pre>
 
       All requests to the API must be sent with this
       <b> userId.</b>
