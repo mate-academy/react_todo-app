e66cd3e51d9b8767da066ea1e9ae6d669becef53
Maks <comanderius@gmail.com>
2023-04-02 12:06:28 +0200

    Fixed LocalSorage

--- src/hooks/LocalStorage.tsx
+++ src/hooks/LocalStorage.tsx
@@ -6,13 +6,9 @@ export const useLocalStorage = (
 ) => {
   const [value, setValue] = useState(() => {
     try {
-      if (localStorage.getItem(key) !== null) {
-        return JSON.parse(localStorage.getItem(key) || '');
-      }
-
-      return undefined;
+      return JSON.parse(localStorage.getItem(key) || '');
     } catch {
-      return undefined;
+      return [];
     }
   });
 
