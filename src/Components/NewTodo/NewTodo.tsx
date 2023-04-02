fc91e7c3836a114828f8c754dca83435151defa2
Taras Baran <baranprivat22@gmail.com>
2023-04-02 23:13:50 +0300

    refactored NewTodo component

--- src/Components/NewTodo/NewTodo.tsx
+++ src/Components/NewTodo/NewTodo.tsx
@@ -3,70 +3,40 @@ import {
   useState,
   FormEvent,
   FocusEvent,
-  useEffect,
-  useRef,
 } from 'react';
 
-import { PostTodo } from '../../types/PostTodo';
-
 type Props = {
-  createNewTodo: (value: string) => Promise<void>;
+  createNewTodo: (value: string) => void;
 };
 
 const NewTodo: React.FC<Props> = ({ createNewTodo }) => {
   const [value, setValue] = useState('');
-  const [isLoading, setIsLoading] = useState(PostTodo.NONE);
-
-  const inputRef = useRef<HTMLInputElement>(null);
-
-  useEffect(() => {
-    if (isLoading !== PostTodo.SUCCESS) {
-      return;
-    }
-
-    inputRef.current?.focus();
-  }, [isLoading]);
 
   const onChangeHandle = (event: ChangeEvent<HTMLInputElement>) => (
     setValue(event.target.value)
   );
 
-  const onSubmitHandle = (
-    e: FormEvent | FocusEvent<HTMLInputElement>,
-    option: PostTodo = PostTodo.SUCCESS,
-  ) => {
+  const onSubmitHandle = (e: FormEvent | FocusEvent) => {
     e.preventDefault();
 
     if (!value.length) {
       return;
     }
 
-    setIsLoading(PostTodo.PENDING);
-    createNewTodo(value)
-      .finally(() => {
-        setIsLoading(option);
-        setValue('');
-      });
+    createNewTodo(value);
+    setValue('');
   };
-
-  const onBlur = (e: FocusEvent<HTMLInputElement>) => {
-    onSubmitHandle(e, PostTodo.NONE);
-  };
-
-  const isDisabledButton = isLoading === PostTodo.PENDING;
 
   return (
     <form onSubmit={onSubmitHandle}>
       <input
-        ref={inputRef}
         type="text"
         data-cy="createTodo"
         className="new-todo"
         placeholder="What needs to be done?"
         value={value}
         onChange={onChangeHandle}
-        onBlur={onBlur}
-        disabled={isDisabledButton}
+        onBlur={onSubmitHandle}
       />
     </form>
   );
