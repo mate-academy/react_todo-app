b9fbf914b7ebb9371b83e276e35cc5df8e66708e
MargaritaChorniak <chorniak.rita@gmail.com>
2023-06-05 17:34:37 +0300

    react todo-app

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,19 @@ import './styles/index.css';
 import './styles/todo-list.css';
 import './styles/filters.css';
 
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import { App } from './App';
+import { TodoApp } from './components/TodoApp';
 
 ReactDOM.render(
-  <App />,
+  <HashRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<TodoApp />} />
+        <Route path="completed" element={<TodoApp />} />
+        <Route path="active" element={<TodoApp />} />
+      </Route>
+    </Routes>
+  </HashRouter>,
   document.getElementById('root'),
 );
