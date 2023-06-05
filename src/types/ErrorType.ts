ff6ad3e346442b83187bc7ef11473ecc8f8b4ec1
Ruslan Yarosh <ruslan.yarosh.96@gmail.com>
2023-06-05 18:26:41 +0300

    Fixes

--- src/types/ErrorType.ts
+++ src/types/ErrorType.ts
@@ -1,8 +1,4 @@
 export enum ErrorType {
   None = '',
-  Loading = 'Unable to load a todo',
-  Add = 'Unable to add a todo',
-  Remove = 'Unable to delete a todo',
   Title = 'Title can\'t be empty',
-  Update = 'Unable to update a todo',
 }
