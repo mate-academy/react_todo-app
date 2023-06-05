27e099f36352c1e1e4588056d6f0d9f4e940d72a
Nikoramo <kanada387@gmail.com>
2023-06-05 14:05:36 +0200

    solution

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,17 @@ import './styles/index.css';
 import './styles/todo-list.css';
 import './styles/filters.css';
 
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { App } from './App';
 
 ReactDOM.render(
-  <App />,
+  <Router>
+    <Routes>
+      <Route>
+        <Route index element={<App />} />
+        <Route path=":filter" element={<App />} />
+      </Route>
+    </Routes>
+  </Router>,
   document.getElementById('root'),
 );
---
+++ src/types/Filter.ts
@@ -0,0 +1,5 @@
+export enum Filter {
+  ALL = 'all',
+  ACTIVE = 'active',
+  COMPLETED = 'completed',
+}
