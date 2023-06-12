5d1dadf812c12a84aa75f1f1a9d51eaecc9570c5
Maksym Tsarenko <makstsar94@gmail.com>
2023-06-12 09:54:25 +0300

    working version

--- src/App.tsx
+++ src/App.tsx
@@ -1,93 +1,107 @@
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
+import React, { useState, useMemo } from 'react';
+import { TodosFilter } from './TodosFilter';
+import { Header } from './Header';
+import { TodoList } from './TodoList';
+import { Status } from './types/Status';
+import { useLocalStorage } from './helpers/useLocaleStorage';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useLocalStorage('todos', []);
+  const [todoTitle, setTodoTitle] = useState('');
+  const [todoStatus, setTodoStatus] = useState<Status>(Status.All);
+
+  const addTodo = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (todoTitle.trim() !== '') {
+      const newTodo = {
+        id: +new Date().getTime(),
+        title: todoTitle,
+        completed: false,
+      };
+
+      setTodos(prevTodos => [...prevTodos, newTodo]);
+      setTodoTitle('');
+    }
+  };
+
+  const deleteTodo = (todoId: number) => {
+    setTodos(prevTodos => prevTodos.filter(todo => todo.id !== todoId));
+  };
+
+  const clearCompletedTodos = () => {
+    setTodos(prevTodos => prevTodos.filter(todo => !todo.completed));
+  };
+
+  const changeTodoTitle = (todoId: number, newTodoTitle: string) => {
+    setTodos(prevTodos => prevTodos.map(todo => (
+      todo.id === todoId
+        ? { ...todo, title: newTodoTitle }
+        : todo
+    )));
+  };
+
+  const toggleCompleteTodos = (todoId: number, completed: boolean) => {
+    setTodos(prevTodos => prevTodos.map(todo => (
+      todo.id === todoId
+        ? { ...todo, completed: !completed }
+        : todo
+    )));
+  };
+
+  const filteredTodos = useMemo(() => {
+    switch (todoStatus) {
+      case (Status.Active):
+        return todos.filter(todo => !todo.completed);
+
+      case (Status.Completed):
+        return todos.filter(todo => todo.completed);
+
+      default:
+        return todos;
+    }
+  }, [todos, todoStatus]);
+
+  const isAllCompleted = todos.every(todo => todo.completed);
+
+  const toggleAllCopleteTodos = () => {
+    setTodos(prevTodos => prevTodos.map(todo => ({
+      ...todo,
+      completed: !isAllCompleted,
+    })));
+  };
+
   return (
     <div className="todoapp">
-      <header className="header">
-        <h1>todos</h1>
 
-        <form>
-          <input
-            type="text"
-            data-cy="createTodo"
-            className="new-todo"
-            placeholder="What needs to be done?"
+      <Header
+        addTodo={addTodo}
+        todoTitle={todoTitle}
+        setTodoTitle={setTodoTitle}
+      />
+
+      {todos.length > 0 && (
+        <>
+          <TodoList
+            todos={filteredTodos}
+            deleteTodo={deleteTodo}
+            toggleCompleteTodos={toggleCompleteTodos}
+            changeTodoTitle={changeTodoTitle}
+            toggleAllCompleteTodos={toggleAllCopleteTodos}
+            isAllCompleted={isAllCompleted}
+
           />
-        </form>
-      </header>
 
-      <section className="main">
-        <input
-          type="checkbox"
-          id="toggle-all"
-          className="toggle-all"
-          data-cy="toggleAll"
-        />
-        <label htmlFor="toggle-all">Mark all as complete</label>
+          <TodosFilter
+            todos={todos}
+            todoStatus={todoStatus}
+            setTodoStatus={setTodoStatus}
+            clearCompletedTodos={clearCompletedTodos}
+          />
+        </>
+      )}
 
-        <ul className="todo-list" data-cy="todoList">
-          <li>
-            <div className="view">
-              <input type="checkbox" className="toggle" id="toggle-view" />
-              <label htmlFor="toggle-view">asdfghj</label>
-              <button type="button" className="destroy" data-cy="deleteTodo" />
-            </div>
-            <input type="text" className="edit" />
-          </li>
-
-          <li className="completed">
-            <div className="view">
-              <input type="checkbox" className="toggle" id="toggle-completed" />
-              <label htmlFor="toggle-completed">qwertyuio</label>
-              <button type="button" className="destroy" data-cy="deleteTodo" />
-            </div>
-            <input type="text" className="edit" />
-          </li>
-
-          <li className="editing">
-            <div className="view">
-              <input type="checkbox" className="toggle" id="toggle-editing" />
-              <label htmlFor="toggle-editing">zxcvbnm</label>
-              <button type="button" className="destroy" data-cy="deleteTodo" />
-            </div>
-            <input type="text" className="edit" />
-          </li>
-
-          <li>
-            <div className="view">
-              <input type="checkbox" className="toggle" id="toggle-view2" />
-              <label htmlFor="toggle-view2">1234567890</label>
-              <button type="button" className="destroy" data-cy="deleteTodo" />
-            </div>
-            <input type="text" className="edit" />
-          </li>
-        </ul>
-      </section>
-
-      <footer className="footer">
-        <span className="todo-count" data-cy="todosCounter">
-          3 items left
-        </span>
-
-        <ul className="filters">
-          <li>
-            <a href="#/" className="selected">All</a>
-          </li>
-
-          <li>
-            <a href="#/active">Active</a>
-          </li>
-
-          <li>
-            <a href="#/completed">Completed</a>
-          </li>
-        </ul>
-
-        <button type="button" className="clear-completed">
-          Clear completed
-        </button>
-      </footer>
     </div>
   );
 };
---
+++ src/TodoItem.tsx
@@ -0,0 +1,121 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import React, { useState, useEffect, useRef } from 'react';
+import classNames from 'classnames';
+import { Todo } from './types/Todo';
+
+type Props = {
+  todo: Todo,
+  deleteTodo: (todoId: number) => void,
+  toggleCompleteTodos: (todoId: number, completed: boolean) => void,
+  changeTodoTitle: (todoId: number, newTitle: string) => void,
+};
+
+export const TodoItem: React.FC<Props> = ({
+  deleteTodo, toggleCompleteTodos, todo,
+  changeTodoTitle,
+}) => {
+  const { id, title, completed } = todo;
+  const [newTitle, setNewTitle] = useState(title);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  useEffect(() => {
+    if (isEditing) {
+      inputRef.current?.focus();
+    }
+  }, [isEditing]);
+
+  const handleDeleTodo = () => {
+    deleteTodo(id);
+  };
+
+  const handleCompleteTodo = () => {
+    toggleCompleteTodos(id, completed);
+  };
+
+  const handleDoubleClick = () => {
+    setIsEditing(true);
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewTitle(event.target.value);
+  };
+
+  const handleOnBlur = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    if (newTitle !== title) {
+      changeTodoTitle(id, newTitle);
+      setIsEditing(false);
+    }
+
+    if (!newTitle.length) {
+      deleteTodo(id);
+    }
+
+    setIsEditing(false);
+  };
+
+  const handleKeyUp = (event: React.KeyboardEvent) => {
+    switch (event.key) {
+      case 'Enter':
+        changeTodoTitle(id, newTitle);
+        setIsEditing(false);
+        break;
+
+      case 'Escape':
+        setNewTitle(title);
+        setIsEditing(false);
+        break;
+
+      default: break;
+    }
+  };
+
+  return (
+    <li
+      className={classNames({
+        completed,
+        editing: isEditing,
+      })}
+      key={id}
+    >
+      <div className="view">
+        <input
+          checked={completed}
+          onChange={handleCompleteTodo}
+          type="checkbox"
+          className="toggle"
+        />
+        <label
+          onDoubleClick={handleDoubleClick}
+        >
+          {title}
+        </label>
+
+        <button
+          onClick={handleDeleTodo}
+          type="button"
+          className="destroy"
+          data-cy="deleteTodo"
+        />
+      </div>
+
+      <form
+        onSubmit={handleOnBlur}
+      >
+        <input
+          ref={inputRef}
+          value={newTitle}
+          type="text"
+          className="edit"
+          onKeyUp={handleKeyUp}
+          onBlur={handleOnBlur}
+          onChange={handleInputChange}
+        />
+      </form>
+
+    </li>
+  );
+};
