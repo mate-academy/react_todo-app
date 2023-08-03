2a6ccc7879903a581e76a4c07c116fca2ac2c156
zipic <zpc23041988@gmail.com>
2023-08-03 20:52:59 +0300

    changed

--- src/components/todo_filter/TodosFilter.tsx
+++ src/components/todo_filter/TodosFilter.tsx
@@ -22,57 +22,52 @@ export const TodosFilter = () => {
     });
   };
 
-  return (
-    <>
-      {todos.length > 0
-    && (
-      <footer className="footer">
-        <span className="todo-count" data-cy="todosCounter">
-          {`${countActiveTodos}
-          item${countActiveTodos !== 1 ? 's' : ''} left`}
-        </span>
-        <ul className="filters" data-cy="todosFilter">
-          <li>
-            <a
-              href="#/"
-              className={cn(selectFiler === Status.All ? 'selected' : '')}
-              onClick={() => handlerFiler(Status.All)}
-            >
-              All
-            </a>
-          </li>
-
-          <li>
-            <a
-              href="#/active"
-              className={cn(selectFiler === Status.Active ? 'selected' : '')}
-              onClick={() => handlerFiler(Status.Active)}
-            >
-              Active
-            </a>
-          </li>
-
-          <li>
-            <a
-              href="#/completed"
-              className={cn(selectFiler === Status.Completed ? 'selected' : '')}
-              onClick={() => handlerFiler(Status.Completed)}
-            >
-              Completed
-            </a>
-          </li>
-        </ul>
-        {countCompletedTodos > 0 && (
-          <button
-            type="button"
-            className="clear-completed"
-            onClick={clearAllCompleted}
+  return todos.length > 0 && (
+    <footer className="footer">
+      <span className="todo-count" data-cy="todosCounter">
+        {`${countActiveTodos}
+        item${countActiveTodos !== 1 ? 's' : ''} left`}
+      </span>
+      <ul className="filters" data-cy="todosFilter">
+        <li>
+          <a
+            href="#/"
+            className={cn(selectFiler === Status.All ? 'selected' : '')}
+            onClick={() => handlerFiler(Status.All)}
           >
-            Clear completed
-          </button>
-        )}
-      </footer>
-    )}
-    </>
+            All
+          </a>
+        </li>
+
+        <li>
+          <a
+            href="#/active"
+            className={cn(selectFiler === Status.Active ? 'selected' : '')}
+            onClick={() => handlerFiler(Status.Active)}
+          >
+            Active
+          </a>
+        </li>
+
+        <li>
+          <a
+            href="#/completed"
+            className={cn(selectFiler === Status.Completed ? 'selected' : '')}
+            onClick={() => handlerFiler(Status.Completed)}
+          >
+            Completed
+          </a>
+        </li>
+      </ul>
+      {countCompletedTodos > 0 && (
+        <button
+          type="button"
+          className="clear-completed"
+          onClick={clearAllCompleted}
+        >
+          Clear completed
+        </button>
+      )}
+    </footer>
   );
 };
--- src/components/todo_lilst/TodoList.tsx
+++ src/components/todo_lilst/TodoList.tsx
@@ -5,26 +5,22 @@ import { TodosContext } from '../../providers/TodosContext';
 export const TodoList = () => {
   const { filteredTodos, allCompleted, todos } = useContext(TodosContext);
 
-  return (
-    <>
-      {todos.length > 0 && (
-        <section className="main">
-          <input
-            type="checkbox"
-            data-cy="toggleAll"
-            className="toggle-all"
-            id="toggle-all"
-            onChange={allCompleted}
-          />
-          <label htmlFor="toggle-all">Mark all as complete</label>
+  return todos.length > 0 && (
+    <section className="main">
+      <input
+        type="checkbox"
+        data-cy="toggleAll"
+        className="toggle-all"
+        id="toggle-all"
+        onChange={allCompleted}
+      />
+      <label htmlFor="toggle-all">Mark all as complete</label>
 
-          <ul className="todo-list" data-cy="todosList">
-            {filteredTodos.map((todo) => (
-              <TodoItem key={todo.id} todo={todo} />
-            ))}
-          </ul>
-        </section>
-      )}
-    </>
+      <ul className="todo-list" data-cy="todosList">
+        {filteredTodos.map((todo) => (
+          <TodoItem key={todo.id} todo={todo} />
+        ))}
+      </ul>
+    </section>
   );
 };
