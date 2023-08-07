824ee055d091dbc11be6d4a7a397275c4b2be3b6
Viktoriia Tkachyk <viko66666vs@gmail.com>
2023-08-07 15:13:28 +0300

    Solution

---
+++ src/components/TodosFilter.tsx
@@ -0,0 +1,44 @@
+import React, { useContext } from 'react';
+import { Filter } from '../types/Filter';
+import { TodosContext } from './TodosContext';
+
+export const TodosFilter: React.FC = () => {
+  const { filterType, setFilterType } = useContext(TodosContext);
+
+  const makeSetFilterType = (type: Filter) => (
+    () => setFilterType(type));
+
+  return (
+    <ul className="filters" data-cy="todosFilter">
+      <li>
+        <a
+          href="#/"
+          className={filterType === Filter.ALL ? 'selected' : ''}
+          onClick={makeSetFilterType(Filter.ALL)}
+        >
+          All
+        </a>
+      </li>
+
+      <li>
+        <a
+          href="#/active"
+          className={filterType === Filter.ACTIVE ? 'selected' : ''}
+          onClick={makeSetFilterType(Filter.ACTIVE)}
+        >
+          Active
+        </a>
+      </li>
+
+      <li>
+        <a
+          href="#/completed"
+          className={filterType === Filter.COMPLETED ? 'selected' : ''}
+          onClick={makeSetFilterType(Filter.COMPLETED)}
+        >
+          Completed
+        </a>
+      </li>
+    </ul>
+  );
+};
