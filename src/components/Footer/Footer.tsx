6c2eb1590a626c4746ce156c2ea1964fbb4912d4
Maksym Avramenko <nonste@outlook.com>
2023-08-19 19:03:28 +0300

    1 item not items -fixed

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -24,7 +24,7 @@ export const Footer = () => {
   return (
     <footer className="footer">
       <span className="todo-count" data-cy="todosCounter">
-        {`${amountOfUncompletedTodos} items left`}
+        {`${amountOfUncompletedTodos} ${amountOfUncompletedTodos === 1 ? 'item' : 'items'} left`}
       </span>
 
       <TodosFilter />
