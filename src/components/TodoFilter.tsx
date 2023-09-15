7e2fe043f8c604cb6c41579a32c125858c110297
Yevhenii <zheki2004@gmail.com>
2023-09-15 12:57:38 +0300

    todo app implemented

---
+++ src/components/TodoFilter.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Status } from '../types/Status';
+
+type Props = {
+  filter: Status;
+  setFilter: (filter: Status) => void;
+};
+
+export const TodosFilter: React.FC<Props> = ({ filter, setFilter }) => {
+  const onFilterChange = (status: Status) => () => {
+    setFilter(status);
+  };
+
+  return (
+    <ul className="filters">
+      <li>
+        <a
+          href="#/"
+          className={filter === 'all' ? 'selected' : ''}
+          onClick={onFilterChange(Status.All)}
+        >
+          All
+        </a>
+      </li>
+
+      <li>
+        <a
+          href="#/active"
+          className={filter === 'active' ? 'selected' : ''}
+          onClick={onFilterChange(Status.Active)}
+        >
+          Active
+        </a>
+      </li>
+
+      <li>
+        <a
+          href="#/completed"
+          className={filter === 'completed' ? 'selected' : ''}
+          onClick={onFilterChange(Status.Completed)}
+        >
+          Completed
+        </a>
+      </li>
+    </ul>
+  );
+};
