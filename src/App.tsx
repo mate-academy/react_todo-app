c23c2f0ef16cf1013d1ae5bb398199cd9939537f
Dmytro Fefelov <reseneweb@gmail.com>
2023-10-03 21:22:35 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,59 @@
-/* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
+import React, { useEffect, useState, useContext } from 'react';
+import { TodoList } from './components/TodoList/TodoList';
+import { TodosFilter } from './components/TodosFilter/TodosFilter';
+import { Todo } from './types/Todo';
+import { TodosContext } from './TodosContext';
 
 export const App: React.FC = () => {
+  const { todos, setTodos } = useContext(TodosContext);
+  const [filteredBy, setFilteredBy] = useState('');
+  const [title, setTitle] = useState('');
+  const numberOfNotCompleted = todos.filter(item => !item.completed).length;
+  const [toggleAll, setToggleAll] = useState(false);
+
+  useEffect(() => {
+    setToggleAll(numberOfNotCompleted === 0 && todos.length !== 0);
+  }, [numberOfNotCompleted]);
+
+  const handleToggleAll = () => {
+    setTodos(todos.map(todo => ({
+      ...todo,
+      completed: !toggleAll,
+    })));
+
+    setToggleAll(!toggleAll);
+  };
+
+  const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+  };
+
+  function addTodo(newTodo: Todo) {
+    setTodos([...todos, newTodo]);
+  }
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    if (!title) {
+      return;
+    }
+
+    addTodo({
+      id: todos.length,
+      title,
+      completed: false,
+    });
+
+    setTitle('');
+  };
+
+  const handleClearCompleted = () => {
+    const activeTodos = todos.filter(todo => !todo.completed);
+
+    setTodos(activeTodos);
+  };
+
   return (
     <div className="todoapp">
       <header className="header">
@@ -13,81 +65,58 @@ export const App: React.FC = () => {
             data-cy="createTodo"
             className="new-todo"
             placeholder="What needs to be done?"
+            value={title}
+            onChange={handleTitleChange}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                handleSubmit(e);
+              }
+            }}
           />
         </form>
       </header>
 
       <section className="main">
-        <input
-          type="checkbox"
-          id="toggle-all"
-          className="toggle-all"
-          data-cy="toggleAll"
-        />
-        <label htmlFor="toggle-all">Mark all as complete</label>
+        {(todos.length !== 0) && (
+          <div>
+            <input
+              type="checkbox"
+              id="toggle-all"
+              className="toggle-all"
+              data-cy="toggleAll"
+              checked={toggleAll}
+              onClick={handleToggleAll}
+            />
+            <label htmlFor="toggle-all">Mark all as complete</label>
+          </div>
+        )}
 
-        <ul className="todo-list" data-cy="todoList">
-          <li>
-            <div className="view">
-              <input type="checkbox" className="toggle" id="toggle-view" />
-              <label htmlFor="toggle-view">asdfghj</label>
-              <button type="button" className="destroy" data-cy="deleteTodo" />
-            </div>
-            <input type="text" className="edit" />
-          </li>
-
-          <li className="completed">
-            <div className="view">
-              <input type="checkbox" className="toggle" id="toggle-completed" />
-              <label htmlFor="toggle-completed">qwertyuio</label>
-              <button type="button" className="destroy" data-cy="deleteTodo" />
-            </div>
-            <input type="text" className="edit" />
-          </li>
-
-          <li className="editing">
-            <div className="view">
-              <input type="checkbox" className="toggle" id="toggle-editing" />
-              <label htmlFor="toggle-editing">zxcvbnm</label>
-              <button type="button" className="destroy" data-cy="deleteTodo" />
-            </div>
-            <input type="text" className="edit" />
-          </li>
-
-          <li>
-            <div className="view">
-              <input type="checkbox" className="toggle" id="toggle-view2" />
-              <label htmlFor="toggle-view2">1234567890</label>
-              <button type="button" className="destroy" data-cy="deleteTodo" />
-            </div>
-            <input type="text" className="edit" />
-          </li>
-        </ul>
+        {todos.length !== 0 && (
+          <TodoList
+            filteredBy={filteredBy}
+          />
+        )}
       </section>
 
-      <footer className="footer">
-        <span className="todo-count" data-cy="todosCounter">
-          3 items left
-        </span>
+      {todos.length !== 0 && (
+        <footer className="footer">
+          <span className="todo-count" data-cy="todosCounter">
+            {`${numberOfNotCompleted} item${numberOfNotCompleted !== 1 ? 's' : ''} left`}
+          </span>
 
-        <ul className="filters">
-          <li>
-            <a href="#/" className="selected">All</a>
-          </li>
+          <TodosFilter setFilteredBy={setFilteredBy} />
 
-          <li>
-            <a href="#/active">Active</a>
-          </li>
-
-          <li>
-            <a href="#/completed">Completed</a>
-          </li>
-        </ul>
-
-        <button type="button" className="clear-completed">
-          Clear completed
-        </button>
-      </footer>
+          {(todos.length !== numberOfNotCompleted) && (
+            <button
+              type="button"
+              className="clear-completed"
+              onClick={handleClearCompleted}
+            >
+              Clear completed
+            </button>
+          )}
+        </footer>
+      )}
     </div>
   );
 };
