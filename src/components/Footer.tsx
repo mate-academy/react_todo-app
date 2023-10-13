e6261680bb234e8e6b443b91e0e26692ec884c4d
Maksym Bevskyi <bevskiymaks@gmail.com>
2023-10-13 16:56:33 +0200

    add data-cy

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -42,7 +42,7 @@ export const Footer: React.FC<Props> = React.memo(() => {
         )}
       </span>
 
-      <ul className="filters">
+      <ul className="filters" data-cy="todosList">
         <li>
           <a
             href="#/"
