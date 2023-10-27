11312a7e2a31c706bb8028196add38d9df536e76
Sviatoslav Kondur <sviatikkondur5@gmail.com>
2023-10-27 11:51:26 +0200

    clear completed button

--- src/components/ClearCompletedButton.tsx
+++ src/components/ClearCompletedButton.tsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { TodosContext } from '../contexts/TodosContext';
 
 export const ClearCompletedButton: React.FC = () => {
+  const [todos, setTodos] = useContext(TodosContext);
+
+  const handleClearCompleted = () => {
+    const modifiedTodos = todos.filter(todo => !todo.completed);
+
+    setTodos(modifiedTodos);
+  };
+
   return (
-    <button type="button" className="clear-completed">
+    <button
+      type="button"
+      className="clear-completed"
+      onClick={handleClearCompleted}
+    >
       Clear completed
     </button>
   );
