60157759c36d5b8f920e037d66e33d659b0a8fb7
Sviatoslav Kondur <sviatikkondur5@gmail.com>
2023-10-27 11:25:28 +0200

    todos left count

--- src/components/TodoCount.tsx
+++ src/components/TodoCount.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/naming-convention */
+import React, { useContext } from 'react';
+import { TodosContext } from '../contexts/TodosContext';
 
 export const TodoCount: React.FC = () => {
+  const [todos, _] = useContext(TodosContext);
+  const todosLeft = todos.filter(todo => !todo.completed).length;
+
   return (
     <span className="todo-count" data-cy="todosCounter">
-      3 items left
+      {`${todosLeft} items left`}
     </span>
   );
 };
