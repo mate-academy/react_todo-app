88da5e9a5e529132bdca4fb98f57a2fdd855978e
Sviatoslav Kondur <sviatikkondur5@gmail.com>
2023-10-30 15:18:46 +0100

    implemented adding a todo on form blur

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -34,6 +34,7 @@ export const Header: React.FC = () => {
           className="new-todo"
           placeholder="What needs to be done?"
           onChange={e => setTitle(e.target.value)}
+          onBlur={handleSubmit}
         />
       </form>
     </header>
