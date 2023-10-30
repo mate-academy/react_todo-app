f656c0340696a62f040f980d8b90598321efab23
Sviatoslav Kondur <sviatikkondur5@gmail.com>
2023-10-30 12:44:12 +0100

    simplified toggle all

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,7 +1,6 @@
 import React, { useContext } from 'react';
 import { TodoList } from './TodoList';
 import { TodosContext } from '../contexts/TodosContext';
-import { Todo } from '../types/Todo';
 
 export const Main: React.FC = () => {
   const { todos, setTodos } = useContext(TodosContext);
@@ -9,22 +8,11 @@ export const Main: React.FC = () => {
   const isChecked = todos.every(todo => todo.completed);
 
   const handleToggleAll = () => {
-    let modifiedTodos: Todo[] = [];
-    const areAllSameTodoStatus
-      = todos.every(todo => todo.completed)
-      || todos.every(todo => !todo.completed);
-
-    if (areAllSameTodoStatus) {
-      modifiedTodos = todos.map(todo => ({
-        ...todo,
-        completed: !todo.completed,
-      }));
-    } else {
-      modifiedTodos = todos.map(todo => ({
-        ...todo,
-        completed: true,
-      }));
-    }
+    const isAllCompleted = todos.every(todo => todo.completed);
+    const modifiedTodos = todos.map(todo => ({
+      ...todo,
+      completed: !isAllCompleted,
+    }));
 
     setTodos(modifiedTodos);
   };
