9cc47ec9ed6a3d91da07c4e5858c0edc28faa380
Danylo Artomov <daniilart01@gmail.com>
2023-11-15 20:59:37 +0200

    implementation

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,14 +1,15 @@
-import { useContext } from 'react';
+import { useContext, useCallback } from 'react';
 import { TodosContext } from '../../context/TodosContext';
 import { Todo } from '../../types/todo';
 import { TodoList } from '../TodoList';
+import { Status } from '../../enums/Status';
 
 type Props = {
 
 };
 
 export const Main: React.FC<Props> = () => {
-  const { todos, setTodos } = useContext(TodosContext);
+  const { todos, setTodos, status } = useContext(TodosContext);
 
   const markAllPressed = (todosList: Todo[]) => {
     if (todosList.find(todo => todo.completed === false)) {
@@ -30,6 +31,21 @@ export const Main: React.FC<Props> = () => {
     }
   };
 
+  const filterItems
+    = useCallback((filterOption: Status, itemsList: Todo[]): Todo[] => {
+      return itemsList.filter(item => {
+        if (filterOption === Status.Active) {
+          return !item.completed;
+        }
+
+        if (filterOption === Status.Completed) {
+          return item.completed;
+        }
+
+        return true;
+      });
+    }, []);
+
   return (
     <section className="main">
       <input
@@ -42,7 +58,7 @@ export const Main: React.FC<Props> = () => {
       />
       <label htmlFor="toggle-all">Mark all as complete</label>
 
-      <TodoList items={todos} />
+      {todos.length !== 0 && <TodoList items={filterItems(status, todos)} />}
 
     </section>
   );
