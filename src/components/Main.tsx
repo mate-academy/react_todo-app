4aff763c8104974d9c5086b386d804260f525423
unknown <maksoleksyuk@gmail.com>
2023-12-26 16:48:56 +0200

    remove dot inside button tag

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -5,33 +5,30 @@ import { Todos } from './Todos';
 export const Main = () => {
   const [labelClick, setLabelClick] = useState(0);
   const { state: { todos }, dispatch } = React.useContext(TodosContext);
+
   const handleToggleAll = () => {
     setLabelClick(labelClick + 1);
     dispatch({
-      type: (labelClick % 2)
+      type: labelClick % 2
         ? 'MARK_ALL_AS_UNCOMPLETED' : 'MARK_ALL_AS_COMPLETED',
     });
   };
 
-  if (!todos.length) {
-    return null;
-  }
-
   return (
-    <section className="main">
-      <input
-        onClick={handleToggleAll}
-        type="checkbox"
-        id="toggle-all"
-        className="toggle-all"
-        data-cy="toggleAll"
-      />
-      <label
-        htmlFor="toggle-all"
-      >
-        Mark all as completed
-      </label>
-      <Todos />
-    </section>
+    todos.length > 0 && (
+      <section className="main">
+        <input
+          onClick={handleToggleAll}
+          type="checkbox"
+          id="toggle-all"
+          className="toggle-all"
+          data-cy="toggleAll"
+        />
+        <label htmlFor="toggle-all">
+          Mark all as completed
+        </label>
+        <Todos />
+      </section>
+    )
   );
 };
