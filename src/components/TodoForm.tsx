0ddcb7765d008d8f597ad6badf0e5854c94c30c6
Ivan <fanmetalheavy@gmail.com>
2024-01-19 00:33:48 +0200

    add a check to prevent the entry of empty text

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useCallback, useContext, useState } from 'react';
+import cn from 'classnames';
 import { TodosContext } from './Store';
 import { Todo } from '../types/todo';
 
@@ -7,6 +8,7 @@ type Props = {};
 
 export const TodoForm: React.FC<Props> = () => {
   const [title, setTitle] = useState('');
+  const [error, setError] = useState('');
 
   const { todos, setTodos } = useContext(TodosContext);
 
@@ -21,13 +23,20 @@ export const TodoForm: React.FC<Props> = () => {
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
 
+    if (title.length === 0) {
+      setError('Please add a text');
+
+      return;
+    }
+
     addTodo({
       id: +new Date(),
-      title,
+      title: title.trim(),
       complete: false,
     });
 
     setTitle('');
+    setError('');
   };
 
   return (
@@ -38,8 +47,8 @@ export const TodoForm: React.FC<Props> = () => {
       <input
         type="text"
         data-cy="createTodo"
-        className="new-todo"
-        placeholder="What needs to be done?"
+        className={cn('new-todo', { error })}
+        placeholder={error || 'What needs to be done?'}
         value={title}
         onChange={handleTitle}
       />
