f4cfd64ec0760b80fe4e0d8acd0d85d6f3d5e6f8
Nadiia Romanskaya <97957968+NadiiaR@users.noreply.github.com>
2024-01-19 20:17:27 +0200

    add solution

---
+++ src/components/TodoContext/TodoContext.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { Todo, Status } from '../../types';
+import { useLocalStorage } from '../../hooks/useLocalStorage';
+
+type Props = {
+  children: React.ReactNode;
+};
+
+type ContextType = {
+  todos: Todo[];
+  setTodos: (todoArr: Todo[]) => void;
+  filterStatus: Status;
+  setFilterStatus: React.Dispatch<React.SetStateAction<Status>>
+};
+
+export const TodoContext = React.createContext<ContextType>({
+  todos: [],
+  setTodos: () => { },
+  filterStatus: Status.All,
+  setFilterStatus: () => { },
+});
+
+export const TodoProvider: React.FC<Props> = ({ children }) => {
+  const [todos, setTodos] = useLocalStorage<Todo[]>('todos', []);
+  const [filterStatus, setFilterStatus] = useState(Status.All);
+
+  return (
+    <TodoContext.Provider value={{
+      todos,
+      setTodos,
+      filterStatus,
+      setFilterStatus,
+    }}
+    >
+      {children}
+    </TodoContext.Provider>
+  );
+};
---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Todo } from '../../types';
+import { TodoItem } from '../TodoItem';
+
+type Props = {
+  items: Todo[];
+};
+
+export const TodoList: React.FC<Props> = ({ items }) => (
+  <ul className="todo-list" data-cy="todosList">
+    {items.map(todo => <TodoItem key={todo.id} todo={todo} />)}
+  </ul>
+);
