556b56c2fef29cd652a620c803cc5b7ddcc0dd59
vova <hohiop09@gmail.com>
2024-02-23 11:29:57 +0200

    save

--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -21,7 +21,6 @@ export const TodoItem: React.FC<Props> = ({ item }) => {
   return (
     <li className={classNames({ completed: item.completed })}>
       <div className="view">
-        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
         <input
           type="checkbox"
           className="toggle"
@@ -29,6 +28,7 @@ export const TodoItem: React.FC<Props> = ({ item }) => {
           onChange={handleCompleted}
         />
         <label htmlFor={`toggle-view-${item.id}`}>{item.title}</label>
+        {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
         <button
           type="button"
           className="destroy"
