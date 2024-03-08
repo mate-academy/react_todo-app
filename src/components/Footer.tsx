1e0e3e1d311d7e6c50a9a6cb4feace876ef0fa84
Marta Chobaniuk <marta.chobaniuk@gmail.com>
2024-03-08 13:50:50 +0200

    added Todo app

---
+++ src/components/Footer.tsx
@@ -0,0 +1,34 @@
+import { useCallback, useContext, useMemo } from 'react';
+import { TodosContext } from './Contexts';
+import { TodosFilter } from './TodosFilter';
+
+export const Footer: React.FC = () => {
+  const { todos, setTodos } = useContext(TodosContext);
+
+  const todosLeft = useMemo(() => {
+    return todos.filter(todo => !todo.completed).length;
+  }, [todos]);
+
+  const removeCompletedTodos = useCallback(() => {
+    setTodos(todos.filter(prevTodo => !prevTodo.completed));
+  }, [setTodos, todos]);
+
+  return (
+    <footer className="footer">
+      <span className="todo-count" data-cy="todosCounter">
+        {`${todosLeft} items left`}
+      </span>
+      <TodosFilter />
+
+      {todos.some(todo => todo.completed) && (
+        <button
+          type="button"
+          className="clear-completed"
+          onClick={removeCompletedTodos}
+        >
+          Clear completed
+        </button>
+      )}
+    </footer>
+  );
+};
