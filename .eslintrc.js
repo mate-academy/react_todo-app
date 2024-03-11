409b0b6cc8e67fdcc3f3cc697a647ee3069db257
HolyRaveOS <kitlaruk1995@gmail.com>
2024-03-11 14:22:47 +0200

    add uuid dependenci

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,24 @@
 module.exports = {
-  extends: ['@mate-academy/eslint-config-react-typescript', 'plugin:cypress/recommended'],
+  extends: [
+    '@mate-academy/eslint-config-react-typescript',
+    'plugin:cypress/recommended',
+  ],
   rules: {
-    'max-len': ['error', {
-      ignoreTemplateLiterals: true,
-      ignoreComments: true,
-    }],
-    'jsx-a11y/label-has-associated-control': ["error", {
-      assert: "either",
-    }],
+    'max-len': [
+      'error',
+      {
+        ignoreTemplateLiterals: true,
+        ignoreComments: true,
+      },
+    ],
+    'jsx-a11y/label-has-associated-control': [
+      'error',
+      {
+        assert: 'either',
+      },
+    ],
+    'operator-linebreak': 0,
+    'import/no-cycle': 0,
+    '@typescript-eslint/indent': 0,
   },
 };
