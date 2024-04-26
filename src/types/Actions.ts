871db21b88748819bf383bc90d611e50b8736cb8
Александр <0999353523ak@gmail.com>
2024-04-26 16:53:56 +0300

    Solution

---
+++ src/types/Actions.ts
@@ -0,0 +1,14 @@
+export enum FilterAction {
+  All = 'ALL',
+  Active = 'ACTIVE',
+  Completed = 'COMPLETED',
+}
+
+export enum ListAct {
+  Add = 'ADD',
+  Delete = 'DELETE',
+  Update = 'UPDATE',
+  SetAllComplet = 'SET_ALL_COMPLET',
+  SetComplet = 'SET_COMPLET',
+  ClearComplet = 'CLEAR_COMPLET',
+}
