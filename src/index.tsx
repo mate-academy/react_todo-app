73a4c676857c8fce39719188d4c7e87be2d6b607
Dmytro Popovych <dmytro.popovych.dev@gmail.com>
2024-05-22 16:58:09 +0200

    Solution

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
+import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-import './styles/index.css';
-import './styles/todo-list.css';
-import './styles/filters.css';
+import './styles/index.scss';
 
 import { App } from './App';
 
 const container = document.getElementById('root') as HTMLDivElement;
 
-createRoot(container).render(<App />);
+createRoot(container).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
