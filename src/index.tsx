395bca16dfe7d67105b3502e78be0ad808768dcf
Volodymyr Isaiev <isaiev.v.o@gmail.com>
2024-07-11 14:43:57 +0300

    Add todo list

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import { createRoot } from 'react-dom/client';
+import { GlobalStateProvider } from './context/StateContext';
 
-import './styles/index.css';
-import './styles/todo-list.css';
-import './styles/filters.css';
+import './styles/index.scss';
 
 import { App } from './App';
 
 const container = document.getElementById('root') as HTMLDivElement;
 
-createRoot(container).render(<App />);
+createRoot(container).render(
+  <GlobalStateProvider>
+    <App />
+  </GlobalStateProvider>,
+);
