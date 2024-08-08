b051d34ed82790eb7ddb586be65658bf9ff6e2bd
Yurii <urabelivr@gmail.com>
2024-08-08 11:43:30 +0200

    create getStateInitialValue function

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,21 +1,26 @@
+// prettier-ignore
 import { useState } from 'react';
 
+const getStateInitialValue = <T>(key: string, defaultValue: T) => {
+  const savedValue = localStorage.getItem(key);
+
+  if (savedValue === null) {
+    return defaultValue;
+  }
+
+  try {
+    return JSON.parse(savedValue);
+  } catch (error) {
+    localStorage.removeItem(key);
+
+    return defaultValue;
+  }
+};
+
 export function useLocalStorage<T>(key: string, defaultValue: T) {
-  const [value, setValue] = useState(() => {
-    const savedValue = localStorage.getItem(key);
-
-    if (savedValue === null) {
-      return defaultValue;
-    }
-
-    try {
-      return JSON.parse(savedValue);
-    } catch (error) {
-      localStorage.removeItem(key);
-
-      return defaultValue;
-    }
-  });
+  const [value, setValue] = useState(
+    getStateInitialValue<T>(key, defaultValue),
+  );
 
   const save = (newValue: T) => {
     if (Array.isArray(newValue) && newValue.length === 0) {
