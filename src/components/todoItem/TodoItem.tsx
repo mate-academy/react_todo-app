b22c6e1b62aefcfb4ada0f096b43a5f58a7f6e73
Katya <katya.stelmakhova@gmail.com>
2024-08-30 14:45:45 +0200

    delete comments

--- src/components/todoItem/TodoItem.tsx
+++ src/components/todoItem/TodoItem.tsx
@@ -129,39 +129,6 @@ export const TodoItem: FC<Props> = ({
           </button>
         )}
       </div>
-
-      {/* This todo is being edited
-      <div data-cy="Todo" className="todo">
-        <label className="todo__status-label">
-          <input
-            data-cy="TodoStatus"
-            type="checkbox"
-            className="todo__status"
-          />
-        </label>
-
-         This form is shown instead of the title and remove button
-
-      </div>
-
-       This todo is in loadind state
-      <div data-cy="Todo" className="todo">
-        <label className="todo__status-label">
-          <input
-            data-cy="TodoStatus"
-            type="checkbox"
-            className="todo__status"
-          />
-        </label>
-
-        <span data-cy="TodoTitle" className="todo__title">
-          Todo is being saved now
-        </span>
-
-        <button type="button" className="todo__remove" data-cy="TodoDelete">
-          ×
-        </button>
-      </div>*/}
     </>
   );
 };
