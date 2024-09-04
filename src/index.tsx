5a5e1a94dd97a3032df3dde913eb34c557fc8233
Andrii Zakharenko <andriizakharenko@gmail.com>
2024-09-04 12:38:57 +0300

    updated

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,6 @@
 import { createRoot } from 'react-dom/client';
-
-import './styles/index.scss';
-// import './styles/todo-list.css';
-// import './styles/filters.css';
-
 import { App } from './App';
-import React from 'react';
+import './styles/index.scss';
 
 const container = document.getElementById('root') as HTMLDivElement;
 
