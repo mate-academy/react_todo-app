92ff03b97516e58c646e6e233b7fedff3b6b8d8d
Dmytro Loboda <dmytro.loboda@gmail.com>
2024-09-25 06:36:58 +0800

    some improvements to pass test

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useRef } from 'react';
+import React, { useContext, useRef } from 'react';
 import { DispatchContext, StateContext } from '../Storage/storageFiles';
 import cn from 'classnames';
 
