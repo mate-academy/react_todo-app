18db7b28fd0028654b9373b4dd745c6ca81d4a13
Maksym Mohyla <greenparksquad@gmail.com>
2024-11-07 08:45:05 +0200

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 import { Header } from './components/Header';
 import { TodoList } from './components/TodoList';
 import { Footer } from './components/Footer';
