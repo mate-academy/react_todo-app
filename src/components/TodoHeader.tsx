58cc9ba1176ad920967fcddc28490d051d6067b9
Mykola Melnichuk <nikolfutdeveloper@gmail.com>
2025-02-25 20:19:58 +0100

    made solution

--- src/components/TodoHeader.tsx
+++ src/components/TodoHeader.tsx
@@ -10,7 +10,7 @@ export const TodoHeader: React.FC<TodoHeaderProps> = () => {
 
   return (
     <header className="todoapp__header">
-      {/* this button should have `active` class only if all todos are completed */}
+      {/* this button should have `active`  class only if all todos are completed */}
       <button
         type="button"
         className="todoapp__toggle-all active"
