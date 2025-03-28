3d91497add192e7a63bc7bc3a83a45aedbf58c76
Burchak Dmitry <burchak.dmitry@icloud.com>
2025-03-28 16:39:00 +0200

    made edits

--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -74,7 +74,7 @@ export const TodoItem: React.FC<Props> = ({ todo }) => {
     }
 
     reset();
-  }, [editText, title, todo, id, setTodos, inputFocus, onDelete, reset]);
+  }, [editText, todo, id, setTodos, onDelete, reset]);
 
   return (
     <div data-cy="Todo" className={classNames('todo', { completed })}>
