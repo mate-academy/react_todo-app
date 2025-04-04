c39c84505e71c1e845ac612a54bd296b69819ab7
Admin <litvinenkogosha2@gmail.com>
2025-04-04 21:52:13 +0200

    start

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable jsx-a11y/control-has-associated-label */
 import React from 'react';
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './styles/index.scss';
 
 import { App } from './App';
+import React from 'react';
 
 const container = document.getElementById('root') as HTMLDivElement;
 
