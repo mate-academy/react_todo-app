14230485b4188bab177316e105c22b98453820bb
Daria Kondratiuk <kondratiuk.dasha28@gmail.com>
2025-04-18 14:11:29 -0400

    fixed test 2

--- src/App.tsx
+++ src/App.tsx
@@ -11,27 +11,6 @@ export const App: React.FC = () => {
   const [filter, setFilter] = useState<Filter>(Filter.All);
 
   const filteredTodos = getFilteredTodos(todos, filter);
-  //   setLoadingByIds(prev => [...prev, updatedTodo.id]);
-
-  //   if (updatedTodo.title.trim() === '') {
-  //     return deleteTodo(updatedTodo.id);
-  //   }
-
-  //   return todoServices
-  //     .updateTodo(updatedTodo.id, { title: updatedTodo.title })
-  //     .then(newTodo => {
-  //       setTodos(current =>
-  //         current.map(todo => (todo.id === newTodo.id ? newTodo : todo)),
-  //       );
-  //     })
-  //     .catch(error => {
-  //       setErrorMessage(ErrorType.UPDATE);
-  //       throw error;
-  //     })
-  //     .finally(() => {
-  //       setLoadingByIds(prev => prev.filter(id => id !== updatedTodo.id));
-  //     });
-  // };
 
   return (
     <div className="todoapp">
@@ -41,8 +20,6 @@ export const App: React.FC = () => {
         <TodoForm />
 
         <TodoList filteredTodos={filteredTodos} />
-
-        {/* Hide the footer if there are no todos */}
 
         {todos.length > 0 && <Footer filter={filter} setFilter={setFilter} />}
       </div>
