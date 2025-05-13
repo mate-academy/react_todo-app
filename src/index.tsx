897a4680bc78ef41b8ff3dc3a7285dcacc70cd62
RomanHasiuk <romanhasiuk1@gmail.com>
2025-05-13 22:11:14 +0300

    add task solution

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,15 @@ import { createRoot } from 'react-dom/client';
 import './styles/index.scss';
 
 import { App } from './App';
+import { TodosProvider } from './context/TodosContext';
+import { FilterProvider } from './context/FilteredTodosContext';
 
 const container = document.getElementById('root') as HTMLDivElement;
 
-createRoot(container).render(<App />);
+createRoot(container).render(
+  <TodosProvider>
+    <FilterProvider>
+      <App />
+    </FilterProvider>
+  </TodosProvider>,
+);
