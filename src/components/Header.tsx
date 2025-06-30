7ef32be6eebb5767e072cc2d8220d2baf2d01503
Bernavschi Natalia <natb@list.ru>
2025-06-30 08:35:39 +0300

    fix some errors

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -32,7 +32,7 @@ export const Header: React.FC = () => {
           className={`todoapp__toggle-all ${allCompleted ? 'active' : ''}`}
           data-cy="ToggleAllButton"
           onClick={toggleAll}
-        />
+        ></button>
       )}
 
       <form onSubmit={handleSubmit}>
