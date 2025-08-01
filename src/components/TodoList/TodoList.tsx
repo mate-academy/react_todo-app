8034f416ad23afea39589bb8c22e6bcadcf519c0
Hanna <hannachachacha7@gmail.com>
2025-08-01 14:09:06 +0300

    add minor fix

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable jsx-a11y/control-has-associated-label */
 import { useState, useRef, useEffect } from 'react';
 import { useTodos } from '../../contexts/TodosContext';
 import '../../styles/todo-list.scss';
