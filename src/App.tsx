16a079bbd9eb9f36e42b37d8c6af6d1d54141e84
Albina <yakunina.albina@gmail.com>
2025-08-11 15:26:40 +0200

    new commit with import

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 /* eslint-disable max-len */
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import { TodoappContent } from './components/TodoappContent';
+import { TodoAppContent } from './components/TodoAppContent';
 
 export const App: React.FC = () => {
   return (
     <div className="todoapp">
       <h1 className="todoapp__title">todos</h1>
 
-      <TodoappContent />
+      <TodoAppContent />
     </div>
   );
 };
