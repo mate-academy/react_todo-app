a290bdf366de167f2f82a7a2e3e89b635e75fa2c
Aweiro <arsenskott@gmail.com>
2025-09-24 01:13:42 +0300

    add reducer with inside methods

--- src/context/TodoContext.tsx
+++ src/context/TodoContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useEffect, useMemo, useState } from 'react';
+import React, { createContext, useEffect, useMemo, useReducer } from 'react';
 import { Todo } from '../types/Todo';
 import { useLocalStorage } from '../hooks/UseLocalStorage';
 import { FilterTypes } from '../types/FilterTypes';
@@ -25,77 +25,114 @@ export const TodoContext = createContext<TodoContextType>({
   filteredTodos: [],
 });
 
-export const TodoProvider: React.FC<{
-  children: React.ReactNode;
-}> = ({ children }) => {
-  const [todos, setTodos] = useLocalStorage<Todo[]>('todos', []);
-  const [disabledButton, setDisabledButton] = useState(true);
-  const [selectedFilter, setSelectedFilter] = useState<FilterTypes>(
-    FilterTypes.All,
-  );
+type InitialState = {
+  selectedFilter: FilterTypes;
+  todos: Todo[];
+};
 
-  useEffect(() => {
-    const isAllCompleted =
-      [...todos].filter(todo => todo.completed).length === todos.length;
+type Action =
+  | { type: 'ADD_TODO'; payload: Todo }
+  | { type: 'DELETE_TODO'; payload: number }
+  | { type: 'UPDATE_TODO'; payload: Todo }
+  | { type: 'SET_FILTER'; payload: FilterTypes };
 
-    if (isAllCompleted) {
-      setDisabledButton(true);
-    } else {
-      setDisabledButton(false);
-    }
-  }, [todos, setDisabledButton]);
-
-  const filteredTodos = useMemo(() => {
-    if (selectedFilter === 'Active') {
-      return [...todos].filter(todo => !todo.completed);
-    }
-
-    if (selectedFilter === 'Completed') {
-      return [...todos].filter(todo => todo.completed);
-    }
-
-    return todos;
-  }, [selectedFilter, todos]);
-
-  const value = useMemo(
-    () => ({
-      todos,
-      setTodos,
-      addTodo: (newTodo: Todo) => {
-        setTodos([...todos, newTodo]);
-      },
-
-      deleteTodo: (todoId: number) => {
-        setTodos(prev => prev.filter(todo => todo.id !== todoId));
-      },
-
-      updateTodos: (todoToUpdate: Todo) => {
-        setTodos(prev => {
-          return prev.map(currentTodo => {
-            if (currentTodo.id === todoToUpdate.id) {
+function reducer(state: InitialState, action: Action): InitialState {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return { ...state, todos: [...state.todos, action.payload] };
+    case 'DELETE_TODO':
+      return {
+        ...state,
+        todos: [...state.todos.filter(todo => todo.id !== action.payload)],
+      };
+    case 'UPDATE_TODO':
+      return {
+        ...state,
+        todos: [
+          ...state.todos.map(currentTodo => {
+            if (currentTodo.id === action.payload.id) {
               return {
                 ...currentTodo,
-                completed: todoToUpdate.completed,
-                title: todoToUpdate.title,
+                completed: action.payload.completed,
+                title: action.payload.title,
               };
             }
 
             return currentTodo;
-          });
+          }),
+        ],
+      };
+    case 'SET_FILTER':
+      return { ...state, selectedFilter: action.payload };
+    default:
+      return state;
+  }
+}
+
+export const TodoProvider: React.FC<{
+  children: React.ReactNode;
+}> = ({ children }) => {
+  const [todos, setTodos] = useLocalStorage<Todo[]>('todos', []);
+  const [state, dispatch] = useReducer(reducer, {
+    todos: todos,
+    selectedFilter: FilterTypes.All,
+  });
+
+  useEffect(() => {
+    setTodos(state.todos);
+  }, [setTodos, state.todos]);
+
+  const filteredTodos = useMemo(() => {
+    if (state.selectedFilter === 'Active') {
+      return [...todos].filter(todo => !todo.completed);
+    }
+
+    if (state.selectedFilter === 'Completed') {
+      return [...todos].filter(todo => todo.completed);
+    }
+
+    return todos;
+  }, [state.selectedFilter, todos]);
+
+  const value = useMemo(
+    () => ({
+      todos: state.todos,
+      addTodo: (newTodo: Todo) => {
+        dispatch({
+          type: 'ADD_TODO',
+          payload: newTodo,
         });
       },
 
-      disabledButton: disabledButton,
-
-      setFilterType: (currentFilterType: FilterTypes) => {
-        setSelectedFilter(currentFilterType);
+      deleteTodo: (todoId: number) => {
+        dispatch({
+          type: 'DELETE_TODO',
+          payload: todoId,
+        });
       },
 
-      selectedFilter: selectedFilter,
+      updateTodos: (todoToUpdate: Todo) => {
+        dispatch({
+          type: 'UPDATE_TODO',
+          payload: todoToUpdate,
+        });
+      },
+
+      disabledButton:
+        state.todos.length > 0 && state.todos.every(t => t.completed),
+
+      setFilterType: (currentFilterType: FilterTypes) => {
+        dispatch({
+          type: 'SET_FILTER',
+          payload: currentFilterType,
+        });
+      },
+
+      selectedFilter: state.selectedFilter,
 
       filteredTodos: filteredTodos,
     }),
-    [todos, setTodos, disabledButton, selectedFilter, filteredTodos],
+    [state.todos, state.selectedFilter, filteredTodos],
   );
 
   return <TodoContext.Provider value={value}>{children}</TodoContext.Provider>;
