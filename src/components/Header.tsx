6b1c0c37c8ee0f8a911c2ebd1c2f6fba54d8b4bc
Mykhailo Podaniev <mpodaniev@gmail.com>
2026-02-20 19:31:51 +0200

    add form submission logic and input handling in Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,29 @@
+import React, { useContext, useState } from 'react';
+import { TodoContext } from '../context/TodoContext';
+import { Todo } from '../types/Todo';
+
 export const Header = () => {
+  const [inputText, setInputText] = useState('');
+  const { setTodos } = useContext(TodoContext);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!inputText) {
+      return;
+    }
+
+    const newTodo: Todo = {
+      id: Date.now(),
+      title: inputText.trim(),
+      completed: false,
+    };
+
+    setTodos(prevTodos => [...prevTodos, newTodo]);
+
+    setInputText('');
+  };
+
   return (
     <header className="todoapp__header">
       {/* this button should have `active` class only if all todos are completed */}
@@ -9,12 +34,16 @@ export const Header = () => {
       />
 
       {/* Add a todo on form submit */}
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           data-cy="NewTodoField"
           type="text"
           className="todoapp__new-todo"
           placeholder="What needs to be done?"
+          value={inputText}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            setInputText(e.target.value);
+          }}
         />
       </form>
     </header>
