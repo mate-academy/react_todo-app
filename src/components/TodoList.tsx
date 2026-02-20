bd6fbded3aab01ab2946a0739f61531296fec14f
Mykhailo Podaniev <mpodaniev@gmail.com>
2026-02-20 22:23:13 +0200

    render todos from context in TodoList component

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,3 +1,31 @@
+import { useContext } from 'react';
+import { TodoContext } from '../context/TodoContext';
+
 export const TodoList = () => {
-  return <section className="todoapp__main" data-cy="TodoList"></section>;
+  const { todos } = useContext(TodoContext);
+
+  return (
+    <section className="todoapp__main" data-cy="TodoList">
+      {todos.map(todo => (
+        <div key={todo.id} data-cy="Todo" className="todo">
+          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+          <label className="todo__status-label">
+            <input
+              data-cy="TodoStatus"
+              type="checkbox"
+              className="todo__status"
+            />
+          </label>
+
+          <span data-cy="TodoTitle" className="todo__title">
+            {todo.title}
+          </span>
+
+          <button type="button" className="todo__remove" data-cy="TodoDelete">
+            ×
+          </button>
+        </div>
+      ))}
+    </section>
+  );
 };
