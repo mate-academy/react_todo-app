be920ad61e2282a018cc5438404f30d627a04bff
Vlad Zhovtani <zovtanyy@gmail.com>
2026-02-23 15:09:46 +0200

    a

--- src/context/TodosContext.tsx
+++ src/context/TodosContext.tsx
@@ -122,7 +122,7 @@ export const TodosProvider: React.FC<TodosProviderProps> = ({ children }) => {
     }
   }, [todos, mode]);
 
-  // Handle filter changes based on URL hash
+  // Handle filter changes based on URL has
   useEffect(() => {
     const handleHashChange = () => {
       const hash = window.location.hash.slice(2) || 'all';
