228230de8366077b83b2d5ee32fa708371a1907b
protocolo23 <narashikamaro27@gmail.com>
2026-05-25 14:45:10 -0300

    Solution

---
+++ src/Components/TodoFooter/todoFooter.tsx
@@ -0,0 +1,80 @@
+import classNames from 'classnames';
+
+import React, { useContext } from 'react';
+import { TodoContext } from '../../TodoContext';
+import { Filter } from '../utils/Filter';
+
+type Props = {
+  filter: Filter;
+  onFilterChange: (
+    event: React.MouseEvent<HTMLAnchorElement>,
+    value: Filter,
+  ) => void;
+};
+
+export const TodoFooter: React.FC<Props> = ({ filter, onFilterChange }) => {
+  const { todos, dispatch, mainInputRef } = useContext(TodoContext);
+
+  const focusMain = () => {
+    mainInputRef.current?.focus();
+  };
+
+  const handleClearCompleted = () => {
+    dispatch({
+      type: 'clearCompleted',
+    });
+    focusMain();
+  };
+
+  const filterLinks = [
+    { label: 'All', value: Filter.All, href: '#/', dataCy: 'FilterLinkAll' },
+
+    {
+      label: 'Active',
+      value: Filter.Active,
+      href: '#/active',
+      dataCy: 'FilterLinkActive',
+    },
+
+    {
+      label: 'Completed',
+      value: Filter.Completed,
+      href: '#/completed',
+      dataCy: 'FilterLinkCompleted',
+    },
+  ];
+
+  return (
+    <footer className="todoapp__footer" data-cy="Footer">
+      <span className="todo-count" data-cy="TodosCounter">
+        {todos.filter(todo => !todo.completed).length} items left
+      </span>
+
+      <nav className="filter" data-cy="Filter">
+        {filterLinks.map(({ label, value, href, dataCy }) => (
+          <a
+            key={value}
+            href={href}
+            className={classNames('filter__link', {
+              selected: filter === value,
+            })}
+            data-cy={dataCy}
+            onClick={event => onFilterChange(event, value)}
+          >
+            {label}
+          </a>
+        ))}
+      </nav>
+
+      <button
+        type="button"
+        className="todoapp__clear-completed"
+        data-cy="ClearCompletedButton"
+        disabled={!todos.some(todo => todo.completed)}
+        onClick={handleClearCompleted}
+      >
+        Clear completed
+      </button>
+    </footer>
+  );
+};
---
+++ src/TodoContext.tsx
@@ -0,0 +1,123 @@
+import React, { useEffect, useMemo, useReducer, useRef } from 'react';
+import { Todo } from './types/Todo';
+
+export const TodoContext = React.createContext({
+  todos: [] as Todo[],
+  dispatch: (() => {}) as React.Dispatch<Action>,
+  mainInputRef: { current: null } as React.RefObject<HTMLInputElement>,
+});
+
+type Action =
+  | { type: 'addTodo'; payload: string }
+  | { type: 'deleteTodo'; payload: number }
+  | { type: 'toggleTodo'; payload: number }
+  | { type: 'toggleAll' }
+  | { type: 'clearCompleted' }
+  | { type: 'updateTodo'; payload: { id: number; title: string } };
+
+interface State {
+  todos: Todo[];
+}
+
+function reducer(state: State, action: Action): State {
+  switch (action.type) {
+    case 'addTodo':
+      return {
+        ...state,
+        todos: [
+          ...state.todos,
+          {
+            id: +new Date(),
+            userId: 1,
+            title: action.payload,
+            completed: false,
+          },
+        ],
+      };
+
+    case 'deleteTodo':
+      return {
+        ...state,
+        todos: state.todos.filter(todo => todo.id !== action.payload),
+      };
+
+    case 'toggleTodo':
+      return {
+        ...state,
+        todos: state.todos.map(todo => {
+          if (todo.id !== action.payload) {
+            return todo;
+          }
+
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }),
+      };
+
+    case 'toggleAll': {
+      const allCompleted = state.todos.every(todo => todo.completed);
+
+      return {
+        ...state,
+        todos: state.todos.map(todo => ({
+          ...todo,
+          completed: !allCompleted,
+        })),
+      };
+    }
+
+    case 'clearCompleted':
+      return {
+        ...state,
+        todos: state.todos.filter(todo => !todo.completed),
+      };
+
+    case 'updateTodo':
+      return {
+        ...state,
+        todos: state.todos.map(todo => {
+          if (todo.id !== action.payload.id) {
+            return todo;
+          }
+
+          return {
+            ...todo,
+            title: action.payload.title,
+          };
+        }),
+      };
+
+    default:
+      return state;
+  }
+}
+
+const initialState: State = {
+  todos: JSON.parse(localStorage.getItem('todos') || '[]'),
+};
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export const TodoProvider: React.FC<Props> = ({ children }) => {
+  const [{ todos }, dispatch] = useReducer(reducer, initialState);
+  const mainInputRef = useRef<HTMLInputElement>(null);
+
+  const value = useMemo(
+    () => ({
+      todos,
+      dispatch,
+      mainInputRef,
+    }),
+    [todos],
+  );
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
+  return <TodoContext.Provider value={value}>{children}</TodoContext.Provider>;
+};
