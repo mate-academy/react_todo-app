f0e011bf75b9d2021eff4a187ee6de6e0b4b71de
Vladyslav <vkobeluh@gmail.com>
2026-08-05 19:03:07 +0300

    fix(todo-footer): update the completedTodosAmount calculation

--- src/components/TodoFooter/TodoFooter.tsx
+++ src/components/TodoFooter/TodoFooter.tsx
@@ -4,13 +4,14 @@ import './TodoFooter.scss';
 
 export const TodoFooter = () => {
   const todos = useTodos();
+  const completedTodosAmount = todos.filter(todo => todo.completed).length;
 
   const completedTodosAmount = todos.filter(todo => !todo.completed).length;
 
   return (
     <footer className="todo-footer" data-cy="Footer">
       <span className="todo-footer_count" data-cy="TodosCounter">
-        {completedTodosAmount} items left
+        {todos.length - completedTodosAmount} items left
       </span>
 
       {/* Active link should have the 'selected' class */}
