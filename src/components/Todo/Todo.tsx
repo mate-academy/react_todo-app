e5d39efca02a1c1fca9511e7b8e86e1145525768
Vladyslav <vkobeluh@gmail.com>
2026-08-06 12:01:03 +0300

    feat(todos): implement editing logic

--- src/components/Todo/Todo.tsx
+++ src/components/Todo/Todo.tsx
@@ -2,6 +2,7 @@
 import classNames from 'classnames';
 import './Todo.scss';
 import { useSetTodos } from '../../hooks/useSetTodos';
+import React, { useEffect, useRef, useState } from 'react';
 
 interface Props {
   id: number;
@@ -11,6 +12,8 @@ interface Props {
 
 export const Todo = ({ id, title, completed }: Props) => {
   const setTodos = useSetTodos();
+  const [isEditing, setIsEditing] = useState(false);
+  const [tempTitle, setTempTitle] = useState(title);
 
   const handleToggleCompleted = () => {
     setTodos(currentTodos =>
@@ -20,9 +23,51 @@ export const Todo = ({ id, title, completed }: Props) => {
     );
   };
 
-  const handleDelete = () => {
+  const deleteTodo = () => {
     setTodos(currentTodos => currentTodos.filter(todo => todo.id !== id));
   };
+
+  const updateTodo = () => {
+    setTodos(currentTodos =>
+      currentTodos.map(todo =>
+        todo.id === id ? { ...todo, title: tempTitle.trim() } : todo,
+      ),
+    );
+  };
+
+  const saveChanges = () => {
+    if (tempTitle.trim().length === 0) {
+      deleteTodo();
+
+      return;
+    }
+
+    updateTodo();
+
+    setIsEditing(false);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    saveChanges();
+  };
+
+  const handleEditInputKeyUp = (
+    event: React.KeyboardEvent<HTMLInputElement>,
+  ) => {
+    if (event.key === 'Escape') {
+      setIsEditing(false);
+
+      setTempTitle(title);
+    }
+  };
+
+  const editInputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    editInputRef.current?.focus();
+  }, [isEditing]);
 
   return (
     <div data-cy="Todo" className={classNames('todo', { completed })}>
@@ -36,18 +81,40 @@ export const Todo = ({ id, title, completed }: Props) => {
         />
       </label>
 
-      <span data-cy="TodoTitle" className="todo__title">
-        {title}
-      </span>
+      {isEditing ? (
+        <form onSubmit={handleSubmit}>
+          <input
+            data-cy="TodoTitleField"
+            type="text"
+            className="todo__title-field"
+            placeholder="Empty todo will be deleted"
+            ref={editInputRef}
+            onBlur={() => saveChanges()}
+            onKeyUp={handleEditInputKeyUp}
+            onChange={event => setTempTitle(event.target.value)}
+            value={tempTitle}
+          />
+        </form>
+      ) : (
+        <>
+          <span
+            data-cy="TodoTitle"
+            className="todo__title"
+            onDoubleClick={() => setIsEditing(true)}
+          >
+            {title}
+          </span>
 
-      <button
-        type="button"
-        className="todo__remove"
-        data-cy="TodoDelete"
-        onClick={handleDelete}
-      >
-        ×
-      </button>
+          <button
+            type="button"
+            className="todo__remove"
+            data-cy="TodoDelete"
+            onClick={() => deleteTodo()}
+          >
+            ×
+          </button>
+        </>
+      )}
     </div>
   );
 };
